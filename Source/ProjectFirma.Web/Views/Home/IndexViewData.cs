4ea05eaf6f7b233eabb8c4ec06b2d4bbbf843b4d
John Burns <john.burns@sitkatech.com>
2016-11-21 18:29:03 +0000

    ProjectFirma: Add edit button to home page
    git-svn-id: https://svn.sitkatech.com/svn/sitkatech/trunk/ProjectFirma@129193 0729f40c-f363-46e8-bae2-8dcc587728a9

--- Source/ProjectFirma.Web/Views/Home/IndexViewData.cs
+++ Source/ProjectFirma.Web/Views/Home/IndexViewData.cs
@@ -1,13 +1,21 @@
-﻿using ProjectFirma.Web.Models;
+﻿using LtInfo.Common;
+using ProjectFirma.Web.Models;
+using ProjectFirma.Web.Security;
 
 namespace ProjectFirma.Web.Views.Home
 {
     public class IndexViewData : FirmaViewData
     {
-        public IndexViewData(Person currentPerson,
-            Models.FirmaPage firmaPage) : base(currentPerson, firmaPage)
+        public readonly bool ShowEditButton;
+        public readonly string EditUrl;
+
+        public IndexViewData(Person currentPerson, Models.FirmaPage firmaPage) : base(currentPerson, firmaPage)
         {
             PageTitle = "Clackamas Partnership Project Tracker";
+
+            var permissionCheckResult = new FirmaPageManageFeature().HasPermission(currentPerson, firmaPage);
+            ShowEditButton = permissionCheckResult.HasPermission;
+            EditUrl = SitkaRoute<Controllers.HomeController>.BuildUrlFromExpression(x => x.EditPageContent(FirmaPageTypeEnum.HomePage));
         }
     }
 }
