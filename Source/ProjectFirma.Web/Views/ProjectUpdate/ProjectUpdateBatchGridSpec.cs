934be7aa8c065f5749ba4c21e6ccad1dd5a2e860
John Burns <john.burns@sitkatech.com>
2017-01-26 17:47:39 +0000

    ProjectFirma: Fixed bug
    git-svn-id: https://svn.sitkatech.com/svn/sitkatech/trunk/ProjectFirma@132519 0729f40c-f363-46e8-bae2-8dcc587728a9

--- Source/ProjectFirma.Web/Views/ProjectUpdate/ProjectUpdateBatchGridSpec.cs
+++ Source/ProjectFirma.Web/Views/ProjectUpdate/ProjectUpdateBatchGridSpec.cs
@@ -38,7 +38,7 @@ namespace ProjectFirma.Web.Views.ProjectUpdate
                             null,
                             null,
                             null,
-                            new List<string>() {"btn-firma", "btn-xs"}, false);
+                            new List<string>() {"btn-firma", "btn-xs"});
                     }
                     else if (pub.ProjectUpdateState == ProjectUpdateState.Approved)
                     {
