7c451d36c6ab0e25a1b5cd3841f02a2165342846
Michael Ferrante <michael@sitkatech.com>
2017-06-09 09:48:38 -0700

    [projectfirma] - sql server 2016 update, no change to tables just formatting due to new SMO tools, update from "script-tables".

--- Database/Tables/dbo.FieldDefinitionData.sql
+++ Database/Tables/dbo.FieldDefinitionData.sql
@@ -22,7 +22,7 @@ CREATE TABLE [dbo].[FieldDefinitionData](
 	[FieldDefinitionID] ASC,
 	[TenantID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
 GO
 ALTER TABLE [dbo].[FieldDefinitionData]  WITH CHECK ADD  CONSTRAINT [FK_FieldDefinitionData_FieldDefinition_FieldDefinitionID] FOREIGN KEY([FieldDefinitionID])
--- Database/Tables/dbo.ProjectUpdateBatch.sql
+++ Database/Tables/dbo.ProjectUpdateBatch.sql
@@ -37,7 +37,7 @@ CREATE TABLE [dbo].[ProjectUpdateBatch](
 	[ProjectUpdateBatchID] ASC,
 	[TenantID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
 GO
 ALTER TABLE [dbo].[ProjectUpdateBatch]  WITH CHECK ADD  CONSTRAINT [FK_ProjectUpdateBatch_Person_LastUpdatePersonID_PersonID] FOREIGN KEY([LastUpdatePersonID])
