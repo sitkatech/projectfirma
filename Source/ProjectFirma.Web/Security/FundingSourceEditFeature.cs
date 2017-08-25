648ca9ccdd8d524455aaf2d578469c8bdef9f3ac
Laryea Quaye <laryea.quaye@sitkatech.com>
2017-08-25 14:12:29 -0700

    [projectfirma/#1114] Allow designated RCD users to create, edit, and approve updates to their RCD's projects Project Approvers can add/edit funding sources for their organization

---
+++ Source/ProjectFirma.Web/Security/FundingSourceEditFeature.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using ProjectFirma.Web.Models;
+
+namespace ProjectFirma.Web.Security
+{
+    [SecurityFeatureDescription("Edit {0}", FieldDefinitionEnum.FundingSource)]
+    public class FundingSourceEditFeature : FirmaFeatureWithContext, IFirmaBaseFeatureWithContext<FundingSource>
+    {
+        private readonly FirmaFeatureWithContextImpl<FundingSource> _firmaFeatureWithContextImpl;
+
+        public FundingSourceEditFeature() : base(new List<Role> {Role.Admin, Role.SitkaAdmin, Role.ProjectApprover})
+        {
+            _firmaFeatureWithContextImpl = new FirmaFeatureWithContextImpl<FundingSource>(this);
+            ActionFilter = _firmaFeatureWithContextImpl;
+        }
+
+        public PermissionCheckResult HasPermission(Person person, FundingSource contextModelObject)
+        {
+            if (!HasPermissionByPerson(person))
+            {
+                return new PermissionCheckResult($"You don't have permission to edit or delete {FieldDefinition.FundingSource.GetFieldDefinitionLabel()} {contextModelObject.DisplayName}");
+            }
+
+            if (person.RoleID == Role.ProjectApprover.RoleID && contextModelObject.OrganizationID != person.OrganizationID)
+            {
+                return new PermissionCheckResult($"You don't have permission to edit or delete {FieldDefinition.FundingSource.GetFieldDefinitionLabel()} {contextModelObject.DisplayName} because it does not belong to your {FieldDefinition.Organization.GetFieldDefinitionLabel()}");
+            }
+
+            return new PermissionCheckResult();
+        }
+
+        public void DemandPermission(Person person, FundingSource contextModelObject)
+        {
+            _firmaFeatureWithContextImpl.DemandPermission(person, contextModelObject);
+        }
+    }
+}
