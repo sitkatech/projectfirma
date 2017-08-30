fbf6154a824b683031c3ac006f06bafa25aeae52
Laryea Quaye <laryea.quaye@sitkatech.com>
2017-08-30 16:31:40 -0700

    [projectfirma/#1114] Allow designated RCD users to create, edit, and approve updates to their RCD's projects Fix database bug where field definition is not referenced before it is set

--- Database/ReleaseScript/096 - Add CanApproveProjectOrganization and IsPrimaryContactOrganization field definitions.sql
+++ Database/ReleaseScript/096 - Add CanApproveProjectOrganization and IsPrimaryContactOrganization field definitions.sql
@@ -3,6 +3,8 @@ VALUES
 (12, N'IsPrimaryContactOrganization', N'Is Primary Contact Organization', N'<p>The entity with primary responsibility for organizing, planning, and executing implementation activities for a project or program. This is usually the lead implementer.</p>', 1),
 (13, N'CanApproveProjectsOrganization', N'Can Approve Projects Organization', N'<p>The entity with primary responsibility for approving a project.</p>', 1)
 
+go
+
 insert into dbo.FieldDefinitionData(FieldDefinitionID, TenantID, FieldDefinitionLabel)
 values
 (12, 1, 'Lead Implementer'),
