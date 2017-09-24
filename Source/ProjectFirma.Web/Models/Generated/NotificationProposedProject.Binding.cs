01d8d3094c3d72210b6eb443bafe515084e39d5b
raynardlee <raynardlee@yahoo.com>
2017-09-23 23:32:47 -0700

    [projectfirma/#1172] Add Associated RCD and Lead Implementer to Proposal Basics Accidental check-in of generated files

--- Source/ProjectFirma.Web/Models/Generated/NotificationProposedProject.Binding.cs
+++ Source/ProjectFirma.Web/Models/Generated/NotificationProposedProject.Binding.cs
@@ -8,13 +8,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 using System.Data.Entity.Spatial;
 using System.Linq;
-using System.Net.Mail;
 using System.Web;
-using LtInfo.Common;
 using LtInfo.Common.DesignByContract;
 using LtInfo.Common.Models;
 using ProjectFirma.Web.Common;
-using ProjectFirma.Web.Controllers;
 
 namespace ProjectFirma.Web.Models
 {
