680007f13c37efd5e0a412a7aa0c6e5e2393556e
raynardlee <raynardlee@yahoo.com>
2017-10-01 02:37:39 -0700

    fix issue with not being able to delete proposed project

---
+++ Source/ProjectFirma.Web/Models/ProposedProjectOrganization.cs
@@ -0,0 +1,20 @@
+﻿using LtInfo.Common.Views;
+using ProjectFirma.Web.Common;
+
+namespace ProjectFirma.Web.Models
+{
+    public partial class ProposedProjectOrganization : IAuditableEntity
+    {
+        public string AuditDescriptionString
+        {
+            get
+            {
+                var proposedProject = HttpRequestStorage.DatabaseEntities.AllProposedProjects.Find(ProposedProjectID);
+                var organization = HttpRequestStorage.DatabaseEntities.AllOrganizations.Find(OrganizationID);
+                var proposedProjectName = proposedProject != null ? proposedProject.AuditDescriptionString : ViewUtilities.NotFoundString;
+                var organizationName = organization != null ? organization.AuditDescriptionString : ViewUtilities.NotFoundString;
+                return $"Proposed Project: {proposedProjectName}, Organization: {organizationName}";
+            }
+        }
+    }
+}
