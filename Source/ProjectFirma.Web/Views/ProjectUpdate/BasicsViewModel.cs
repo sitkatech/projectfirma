0375cc85ec0aac3f42c944802bf666ee4687a81a
raynardlee <raynardlee@yahoo.com>
2017-11-14 16:45:06 -0800

    [projectfirma/#1215] Full regression test (November 2017) bootstrapping form inputs in project update wizard; changing basics layout to match ltinfo more

--- Source/ProjectFirma.Web/Views/ProjectUpdate/BasicsViewModel.cs
+++ Source/ProjectFirma.Web/Views/ProjectUpdate/BasicsViewModel.cs
@@ -50,10 +50,10 @@ namespace ProjectFirma.Web.Views.ProjectUpdate
         public int? CompletionYear { get; set; }
 
         [FieldDefinitionDisplay(FieldDefinitionEnum.EstimatedTotalCost)]
-        public Money? EstimatedTotalCost { get; set; }
+        public MoneyWholeNumber? EstimatedTotalCost { get; set; }
 
         [FieldDefinitionDisplay(FieldDefinitionEnum.EstimatedAnnualOperatingCost)]
-        public Money? EstimatedAnnualOperatingCost { get; set; }
+        public MoneyWholeNumber? EstimatedAnnualOperatingCost { get; set; }
 
         [DisplayName("Reviewer Comments")]
         [StringLength(ProjectUpdateBatch.FieldLengths.BasicsComment)]
