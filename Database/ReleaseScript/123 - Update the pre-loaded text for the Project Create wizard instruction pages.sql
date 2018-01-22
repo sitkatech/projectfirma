8f69209ccb07487b3f9f4edd5e0c9bedfc6bfdf7
Nick <nick.padinha@sitkatech.com>
2018-01-22 13:08:36 -0800

    [projectfirma/#1130] Add existing project via a workflow Update the pre-loaded text for the Project Create wizard instruction pages for Clackamas Parternship nd RCD Projects.

---
+++ Database/ReleaseScript/123 - Update the pre-loaded text for the Project Create wizard instruction pages.sql
@@ -0,0 +1,63 @@
+UPDATE dbo.FirmaPage
+SET FirmaPageContent='<p>Use this workflow to input a proposal for a new project. Proposals are typically projects that have not yet been selected for implementation. They may be conceptual, or they may be more fleshed out but awaiting evaluation and prioritization.</p>
+
+<p>The various aspects of your project that must be included in a project proposal can be seen on the left. Several of these sections are optional, but others are required. The required sections are:</p>
+
+<ul>
+	<li><strong>Basics:</strong>&nbsp;You must enter the project&#39;s estimated start and completion dates and estimated costs.</li>
+	<li><strong>Location - Simple:</strong>&nbsp;You must input your project&#39;s location via an interactive map or provide a short location description.</li>
+	<li><strong>Expected Performance Measures:</strong>&nbsp;You must enter at least one expected Performance Measure, or provide a note why this information is not available.</li>
+	<li><strong>Project Themes</strong>&nbsp;You must enter at least one Project Themes that your project will attain or support, and provide a description of how it will do so.</li>
+</ul>
+
+<p>Once all required sections are complete you may submit the proposal for review. Submitting a project proposal will notify the Clackamas Partnership Project Tracker administrators. After review, your proposal will either be approved, or the reviewer will contact you for follow up.</p>
+'
+WHERE FirmaPageID = 141
+
+UPDATE dbo.FirmaPage
+SET FirmaPageContent='<p>Use this workflow to input a proposal for a new project. Proposals are typically projects that have not yet been selected for implementation. They may be conceptual, or they may be more fleshed out but awaiting evaluation and prioritization.</p>
+
+<p>The various aspects of your project that must be included in a project proposal can be seen on the left. Several of these sections are optional, but others are required. The required sections are:</p>
+
+<ul>
+	<li><strong>Basics:</strong>&nbsp;You must enter the project&#39;s estimated start and completion dates and estimated costs.</li>
+	<li><strong>Location - Simple:</strong>&nbsp;You must input your project&#39;s location via an interactive map or provide a short location description.</li>
+	<li><strong>Expected Performance Measures:</strong>&nbsp;You must enter at least one expected Performance Measure, or provide a note why this information is not available.</li>
+	<li><strong>Project Themes</strong>&nbsp;You must enter at least one Project Themes that your project will attain or support, and provide a description of how it will do so.</li>
+</ul>
+
+<p>Once all required sections are complete you may submit the proposal for review. Submitting a project proposal will notify the RCD Project Tracker administrators. After review, your proposal will either be approved, or the reviewer will contact you for follow up.</p>
+'
+WHERE FirmaPageID = 142
+
+UPDATE dbo.FirmaPage
+SET FirmaPageContent='<p>Use this workflow to input a project record for an existing project to is already complete or underway.</p>
+
+<p>There are several forms, visible in the navigation menu to the left, which collect data to ensure a&nbsp;complete project record.&nbsp;Some of these sections are optional, but others are required. The required sections are:</p>
+
+<ul>
+	<li><strong>Basics:</strong>&nbsp;You must enter the project&#39;s estimated start and completion dates and estimated costs. You will be asked to select the project stage (e.g. Planning/Design, Implementation, or Complete).</li>
+	<li><strong>Location - Simple:</strong>&nbsp;You must input your project&#39;s location via an interactive map or provide a short location description.</li>
+	<li><strong>Expected and Reported PMs:&nbsp;</strong>You must enter at least one Performance Measure (PM), or provide a note why this information is not available. Projects in the Planning/Design phase only report the Expected PMs, whereas projects in later stages report both Expected and Reported/Actual PMs.</li>
+	<li><strong>Project Themes</strong>&nbsp;You must enter at least one Project Themes that your project will attain or support, and provide a description of how it will do so.</li>
+</ul>
+
+<p>Once all required sections are complete you may submit your proposal for review. Submitting this project will notify the Clackamas Partnership Project Tracker administrators. An administrator must review and approve the project before it becomes publicly visible</p>
+'
+WHERE FirmaPageID = 285
+
+UPDATE dbo.FirmaPage
+SET FirmaPageContent='<p>Use this workflow to input a project record for an existing project to is already complete or underway.</p>
+
+<p>There are several forms, visible in the navigation menu to the left, which collect data to ensure a&nbsp;complete project record.&nbsp;Some of these sections are optional, but others are required. The required sections are:</p>
+
+<ul>
+	<li><strong>Basics:</strong>&nbsp;You must enter the project&#39;s estimated start and completion dates and estimated costs. You will be asked to select the project stage (e.g. Planning/Design, Implementation, or Complete).</li>
+	<li><strong>Location - Simple:</strong>&nbsp;You must input your project&#39;s location via an interactive map or provide a short location description.</li>
+	<li><strong>Expected and Reported PMs:&nbsp;</strong>You must enter at least one Performance Measure (PM), or provide a note why this information is not available. Projects in the Planning/Design phase only report the Expected PMs, whereas projects in later stages report both Expected and Reported/Actual PMs.</li>
+	<li><strong>Project Themes</strong>&nbsp;You must enter at least one Project Themes that your project will attain or support, and provide a description of how it will do so.</li>
+</ul>
+
+<p>Once all required sections are complete you may submit your proposal for review. Submitting this project will notify the RCD Project Tracker administrators. An administrator must review and approve the project before it becomes publicly visible</p>
+'
+WHERE FirmaPageID = 290
