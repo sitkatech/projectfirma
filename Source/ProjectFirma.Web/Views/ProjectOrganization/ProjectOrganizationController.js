e339a9ebe9dfce47a9e6ea9c7083965ab1db4d50
Nick <nick.padinha@sitkatech.com>
2018-01-23 13:44:05 -0800

    [projectfirma/#1124] Add/update Organizations via Create and Update workflows Refactor: Break out a ProjectOrganizationController.js

---
+++ Source/ProjectFirma.Web/Views/ProjectOrganization/ProjectOrganizationController.js
@@ -0,0 +1,90 @@
+﻿/*-----------------------------------------------------------------------
+<copyright file="ProjectOrganizationController.js" company="Tahoe Regional Planning Agency and Sitka Technology Group">
+Copyright (c) Tahoe Regional Planning Agency and Sitka Technology Group. All rights reserved.
+<author>Sitka Technology Group</author>
+</copyright>
+
+<license>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License <http://www.gnu.org/licenses/> for more details.
+
+Source code is available upon request via <support@sitkatech.com>.
+</license>
+-----------------------------------------------------------------------*/
+angular.module("ProjectFirmaApp", []).controller("ProjectOrganizationController", function ($scope, angularModelAndViewData) {
+    $scope.OrganizationToAdd = null;
+
+    $scope.$watch(function () {
+        jQuery(".selectpicker").selectpicker("refresh");
+    });
+
+    $scope.getAvailableOrganizations = function(primarySimple) {
+        var usedOrganizationIDs = _.map($scope.AngularModel.ProjectOrganizationsViewModelJson.ProjectOrganizations,
+            function(f) {
+                return f.OrganizationID;
+            });
+
+        var filteredList = _.filter($scope.AngularViewData.AllOrganizations,
+            function(f) {
+                return primarySimple.OrganizationID === f.OrganizationID ||
+                    !_.includes(usedOrganizationIDs, f.OrganizationID);
+            });
+
+        return filteredList;
+    };
+
+    $scope.addTopTier = function () {
+        var newPrimary = {
+            OrganizationID: null,
+            RelationshipTypes: []
+        };
+        $scope.AngularModel.ProjectOrganizationsViewModelJson.ProjectOrganizations.push(newPrimary);
+        $scope.addDetailTier(newPrimary);
+    };
+
+    $scope.removeTopTier = function (primarySimple) {
+        Sitka.Methods.removeFromJsonArray($scope.AngularModel.ProjectOrganizationsViewModelJson.ProjectOrganizations,
+            primarySimple);
+    };
+
+    $scope.addDetailTier = function (primarySimple) {
+        if (primarySimple.RelationshipTypes == undefined) {
+            primarySimple.RelationshipTypes = [];
+        }
+        primarySimple.RelationshipTypes.push({});
+    };
+
+    $scope.removeDetailTier = function (primarySimple, detailSimple) {
+        Sitka.Methods.removeFromJsonArray(primarySimple.RelationshipTypes, detailSimple);
+    };
+
+    $scope.updateModel = function(option) {
+        var primarySimple =
+            Sitka.Methods.findElementInJsonArray(
+                $scope.AngularModel.ProjectOrganizationsViewModelJson.ProjectOrganizations,
+                "OrganizationID",
+                option.OrganizationID);
+
+        primarySimple.OrganizationID = Number.parseInt(option.OrganizationID);
+    };
+
+    $scope.validRelationshipTypes = function(organizationID) {
+        var organization =
+            Sitka.Methods.findElementInJsonArray($scope.AngularViewData.AllOrganizations,
+                "OrganizationID",
+                organizationID);
+
+        var valid = organization == null ? [] : organization.ValidRelationshipTypeSimples;
+        return valid;
+    };
+
+    $scope.AngularModel = angularModelAndViewData.AngularModel;
+    $scope.AngularViewData = angularModelAndViewData.AngularViewData;
+});
