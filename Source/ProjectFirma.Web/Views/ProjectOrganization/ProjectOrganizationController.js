391b2f6cc5ee741ab359459f97e9b82d59c49a9f
Nick <nick.padinha@sitkatech.com>
2018-01-23 14:04:19 -0800

    [projectfirma/#1124] Add/update Organizations via Create and Update workflows Can now submit the form

--- Source/ProjectFirma.Web/Views/ProjectOrganization/ProjectOrganizationController.js
+++ Source/ProjectFirma.Web/Views/ProjectOrganization/ProjectOrganizationController.js
@@ -87,4 +87,7 @@ angular.module("ProjectFirmaApp", []).controller("ProjectOrganizationController"
 
     $scope.AngularModel = angularModelAndViewData.AngularModel;
     $scope.AngularViewData = angularModelAndViewData.AngularViewData;
+    $scope.HiddenJsonElementID = angularModelAndViewData.HiddenJsonElementID;
+
+    jQuery("form").submit(function () { jQuery($scope.HiddenJsonElementID).val(JSON.stringify($scope.AngularModel.ProjectOrganizationsViewModelJson)); });
 });
