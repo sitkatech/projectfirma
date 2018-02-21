567e66f41317ec293d94d5f97a7ebd2e78741106
Liz Christeleit <liz.christeleit@sitkatech.com>
2018-02-21 12:35:53 -0800

    [projectfirma/#1246] RCD Results: Turn the results page in to a Dashboard Show links to detail pages

--- Source/ProjectFirma.Web/Views/Results/AccomplishmentsDashboardViewData.cs
+++ Source/ProjectFirma.Web/Views/Results/AccomplishmentsDashboardViewData.cs
@@ -31,6 +31,8 @@ namespace ProjectFirma.Web.Views.Results
     {
         public string OrganizationDashboardSummaryUrl { get; }
         public string OrganizationAccomplishmentsUrl { get; }
+        public string TaxonomyTierTwoDetailUrl { get; }
+        public string OrganizationDetailUrl { get; }
         public string SpendingByOrganizationTypeAndOrganizationUrl { get; }
         public string ParticipatingOrganizationsUrl { get; }
 
@@ -55,6 +57,8 @@ namespace ProjectFirma.Web.Views.Results
             ParticipatingOrganizationsUrl = SitkaRoute<ResultsController>.BuildUrlFromExpression(x => x.ParticipatingOrganizations(UrlTemplate.Parameter1Int));
             OrganizationDashboardSummaryUrl = SitkaRoute<ResultsController>.BuildUrlFromExpression(x => x.OrganizationDashboardSummary(UrlTemplate.Parameter1Int));
             OrganizationAccomplishmentsUrl = SitkaRoute<ResultsController>.BuildUrlFromExpression(x => x.OrganizationAccomplishments(UrlTemplate.Parameter1Int, UrlTemplate.Parameter2Int));
+            TaxonomyTierTwoDetailUrl = SitkaRoute<TaxonomyTierTwoController>.BuildUrlFromExpression(x => x.Detail(UrlTemplate.Parameter1Int));
+            OrganizationDetailUrl = SitkaRoute<OrganizationController>.BuildUrlFromExpression(x => x.Detail(UrlTemplate.Parameter1Int));
             SpendingByOrganizationTypeAndOrganizationUrl = SitkaRoute<ResultsController>.BuildUrlFromExpression(x => x.SpendingByOrganizationTypeByOrganization(UrlTemplate.Parameter1Int, UrlTemplate.Parameter2Int, UrlTemplate.Parameter3Int));
             ProjectStewardOrganizationTypeName = projectStewardOrganizationTypeName;
         }
