24eb6edd8ea9b7c31c9fcf2c2b345f62b2f70aca
Liz Christeleit <liz.christeleit@sitkatech.com>
2018-03-19 10:24:56 -0700

    [projectfirma/#1248] Add Visibility to Organizations' Proposals and Pending Projects Fix bug in demo data and display of reported performance measures when a new subcategory is added

---
+++ Database/ReleaseScript/138 - Fix bad demo data.sql
@@ -0,0 +1,2 @@
+insert into dbo.PerformanceMeasureActualSubcategoryOption ([TenantID], [PerformanceMeasureActualID], [PerformanceMeasureSubcategoryOptionID], [PerformanceMeasureID], [PerformanceMeasureSubcategoryID])
+values (9, 2861, 810, 116, 181)
