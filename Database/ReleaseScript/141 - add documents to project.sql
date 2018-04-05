61bc4db6a6ed2fb92418e0a946c64a5369fa1550
Laryea Quaye <laryea.quaye@sitkatech.com>
2018-04-05 11:40:04 -0700

    [ltinfo/#1119] Notify user when connection to Open Data Hub fails during nightly load Add constraint to project document

--- Database/ReleaseScript/141 - add documents to project.sql
+++ Database/ReleaseScript/141 - add documents to project.sql
@@ -5,6 +5,7 @@ create table dbo.ProjectDocument (
 	FileResourceID int not null constraint FK_ProjectDocument_FileResource_FileResourceID foreign key references dbo.FileResource(FileResourceID),
 	DisplayName varchar(200) not null,
 	[Description] varchar(1000) null,
+	constraint AK_ProjectDocument_ProjectID_FileResourceID unique (ProjectID, FileResourceID),
 	constraint FK_ProjectDocument_DisplayName_ProjectID unique (DisplayName, ProjectID),
 	constraint FK_ProjectDocument_Project_ProjectID_TenantID foreign key (ProjectID, TenantID) references dbo.Project(ProjectID, TenantID),
 	constraint FK_ProjectDocument_FileResource_FileResourceID_TenantID foreign key (FileResourceID, TenantID) references dbo.FileResource(FileResourceID, TenantID)
