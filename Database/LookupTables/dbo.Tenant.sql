b77d22f86d73cc6fe4af4d1b4814e2fa2f9201a8
raynardlee <raynardlee@yahoo.com>
2018-05-16 16:48:21 -0700

    [projectfirma/#1255] Migrate to OAuth Had to give each tenant their own custom Keystone client id and urls

--- Database/LookupTables/dbo.Tenant.sql
+++ Database/LookupTables/dbo.Tenant.sql
@@ -3,12 +3,12 @@ go
 
 insert into dbo.Tenant(TenantID, TenantName, TenantDomain, TenantSubdomain, KeystoneOpenIDClientIdentifier, KeystoneOpenIDClientSecret)
 values 
-(1, 'SitkaTechnologyGroup', 'projectfirma.com', 'sitka', 'ProjectFirma', '6C0D5ACB-EF45-4081-AFDA-754DA37A87BD'),
+(1, 'SitkaTechnologyGroup', 'projectfirma.com', 'sitka', 'SitkaProjectFirma', '6C0D5ACB-EF45-4081-AFDA-754DA37A87BD'),
 (2, 'ClackamasPartnership', 'clackamaspartnership.org', null, 'ClackamasPartnership', '2F404371-D118-4C17-9F5E-C94713CAA1FB'),
 (3, 'RCDProjectTracker', 'rcdprojects.org', null, 'RCDProjectTracker', '1C2E2422-2376-4F83-B75B-7DA2A4B3A106'),
-(4, 'InternationYearOfTheSalmon', 'projectfirma.com', 'iysdemo', 'ProjectFirma', '6C0D5ACB-EF45-4081-AFDA-754DA37A87BD'),
-(5, 'DemoProjectFirma', 'projectfirma.com', 'demo', 'ProjectFirma', '6C0D5ACB-EF45-4081-AFDA-754DA37A87BD'),
-(6, 'PeaksToPeople', 'projectfirma.com', 'peakstopeople', 'ProjectFirma', '6C0D5ACB-EF45-4081-AFDA-754DA37A87BD'),
-(7, 'JohnDayPartnership', 'projectfirma.com', 'johndaydemo', 'ProjectFirma', '6C0D5ACB-EF45-4081-AFDA-754DA37A87BD'),
-(8, 'AshlandForestAllLandsRestorationInitiative', 'projectfirma.com', 'ashlanddemo', 'ProjectFirma', '6C0D5ACB-EF45-4081-AFDA-754DA37A87BD'),
-(9, 'IdahoAssociatonOfSoilConservationDistricts', 'projectfirma.com', 'swcdemo', 'ProjectFirma', '6C0D5ACB-EF45-4081-AFDA-754DA37A87BD')
+(4, 'InternationYearOfTheSalmon', 'projectfirma.com', 'iysdemo', 'InternationYearOfTheSalmon', 'D3BAE523-0D42-471B-BBDD-54342AF22E54'),
+(5, 'DemoProjectFirma', 'projectfirma.com', 'demo', 'DemoProjectFirma', '4A6D4335-4925-4BAF-A0A1-BB87E2C44096'),
+(6, 'PeaksToPeople', 'projectfirma.com', 'peakstopeople', 'PeaksToPeople', 'A187BEF6-EE69-4113-9FBE-7CB1B39463D2'),
+(7, 'JohnDayPartnership', 'projectfirma.com', 'johndaydemo', 'JohnDayPartnership', '6237EFB8-D605-47E1-9719-8D1CD1FEDA60'),
+(8, 'AshlandForestAllLandsRestorationInitiative', 'projectfirma.com', 'ashlanddemo', 'AshlandForestAllLandsRestorationInitiative', '0A6AA4FC-A29A-44FA-8F61-F064D31BB5EE'),
+(9, 'IdahoAssociatonOfSoilConservationDistricts', 'projectfirma.com', 'swcdemo', 'IdahoAssociatonOfSoilConservationDistricts', 'ABC8A0C7-EFDC-4DD8-9540-33C926ECC804')
