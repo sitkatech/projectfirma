20d11e114f6632dc7d663c8188ef4ca5e136a315
raynardlee <raynardlee@yahoo.com>
2018-05-16 14:00:33 -0700

    [projectfirma/#1255] Migrate to OAuth made PeaksToPeople have its own keystone config; used wildcards for other demo ones

--- Database/ReleaseScript/154 - Add tenant open id columns.sql
+++ Database/ReleaseScript/154 - Add tenant open id columns.sql
@@ -4,7 +4,7 @@ GO
 update dbo.Tenant
 set KeystoneOpenIDClientIdentifier = 'ProjectFirma',
 KeystoneOpenIDClientSecret = '6C0D5ACB-EF45-4081-AFDA-754DA37A87BD'
-where TenantID in (1, 4, 5, 6, 7, 8, 9)
+where TenantID in (1, 4, 5, 7, 8, 9)
 
 update dbo.Tenant
 set KeystoneOpenIDClientIdentifier = 'ClackamasPartnership',
@@ -17,6 +17,10 @@ set KeystoneOpenIDClientIdentifier = 'RCDProjectTracker',
 KeystoneOpenIDClientSecret = '1C2E2422-2376-4F83-B75B-7DA2A4B3A106'
 where TenantID = 3
 
+update dbo.Tenant
+set KeystoneOpenIDClientIdentifier = 'PeaksToPeople',
+KeystoneOpenIDClientSecret = 'A187BEF6-EE69-4113-9FBE-7CB1B39463D2'
+where TenantID in (6)
 
 
 
