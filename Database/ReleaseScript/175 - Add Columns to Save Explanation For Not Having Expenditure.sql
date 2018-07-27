c2eb446f9db348fb236c97f16b74c9b91b481609
jenna mccarter <jenna.mccarter@sitkatech.com>
2018-07-27 11:19:58 -0700

    [projectfirma/#1329] Simplify expenditure data entry
    * Revise release scripts for merge

--- Database/ReleaseScript/175 - Add Columns to Save Explanation For Not Having Expenditure.sql
+++ Database/ReleaseScript/175 - Add Columns to Save Explanation For Not Having Expenditure.sql
@@ -1,19 +1,3 @@
-/*
-Alter table dbo.Project
-Add NoExpendituresToReport bit null;
-go
-
-
-Update dbo.Project 
-Set NoExpendituresToReport = 0;
-
-
-
-Alter Table dbo.Project Alter Column NoExpendituresToReport bit not null;
-*/
- 
-
-
 Alter table dbo.Project
 Add NoExpendituresToReportExplanation varchar(max) null;
 
