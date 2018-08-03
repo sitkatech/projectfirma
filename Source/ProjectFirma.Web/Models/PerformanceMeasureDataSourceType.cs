2aafa67594bc803144d862180309e8fd1b3ef959
Nick <nick.padinha@sitkatech.com>
2018-08-03 14:32:33 -0700

    [projectfirma/#1339] Calculate value of Technical Assistance as a PM First attempt at calculating the TAV pm

--- Source/ProjectFirma.Web/Models/PerformanceMeasureDataSourceType.cs
+++ Source/ProjectFirma.Web/Models/PerformanceMeasureDataSourceType.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using MoreLinq;
 using ProjectFirma.Web.Common;
 
 namespace ProjectFirma.Web.Models
@@ -30,7 +31,44 @@ namespace ProjectFirma.Web.Models
         public override List<PerformanceMeasureReportedValue> GetReportedPerformanceMeasureValues(
             PerformanceMeasure performanceMeasure, List<int> projectIDs)
         {
-            return new List<PerformanceMeasureReportedValue>();
+            var technicalAssistanceParameters = HttpRequestStorage.DatabaseEntities.TechnicalAssistanceParameters.ToList();
+            var technicalAssistanceHours = HttpRequestStorage.DatabaseEntities.PerformanceMeasures.SingleOrDefault(x=>x.PerformanceMeasureID == 2147);
+            if (technicalAssistanceHours == null)
+            {
+                return new List<PerformanceMeasureReportedValue>();
+            }
+
+            var technicalAssistanceHoursProvidedGroupedByYear = Project.GetReportedPerformanceMeasureValues(technicalAssistanceHours,
+                projectIDs).Where(x =>
+                x.PerformanceMeasureActualSubcategoryOptions.Select(y => y.PerformanceMeasureSubcategoryOptionID)
+                    .Contains(2935)).GroupBy(x=>x.CalendarYear);
+
+            return technicalAssistanceHoursProvidedGroupedByYear.Select(x =>
+            {
+                var year = x.Key;
+                var technicalAssistanceParameter = technicalAssistanceParameters.SingleOrDefault(y=>y.Year == year);
+                var engineeringHourlyCost = technicalAssistanceParameter?.EngineeringHourlyCost;
+                var otherAssistanceHourlyCost = technicalAssistanceParameter?.OtherAssistanceHourlyCost;
+                var technicalAssistanceValueInYear = 0d;
+                foreach (var performanceMeasureReportedValue in x)
+                {
+                    if (performanceMeasureReportedValue.PerformanceMeasureActualSubcategoryOptions
+                        .Select(y => y.PerformanceMeasureSubcategoryOptionID).Contains(2938))
+                    {
+                        technicalAssistanceValueInYear +=
+                            performanceMeasureReportedValue.ReportedValue.GetValueOrDefault() *
+                            (double) engineeringHourlyCost.GetValueOrDefault();
+                    }
+                    else
+                    {
+                        technicalAssistanceValueInYear +=
+                            performanceMeasureReportedValue.ReportedValue.GetValueOrDefault() *
+                            (double)otherAssistanceHourlyCost.GetValueOrDefault();
+                    }
+                }
+
+                return new PerformanceMeasureReportedValue(performanceMeasure, null, year, technicalAssistanceValueInYear);
+            }).ToList();
         }
     }
 }
