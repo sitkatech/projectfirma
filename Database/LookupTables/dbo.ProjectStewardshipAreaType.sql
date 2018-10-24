3ace599997bc4c17f309e89b907e73f2a20c7a91
Nick <nick.padinha@sitkatech.com>
2018-10-24 10:51:33 -0700

    [projectfirma/#1420] Configure roles for project tracker Field definitions

---
+++ Database/LookupTables/dbo.ProjectStewardshipAreaType.sql
@@ -0,0 +1,7 @@
+delete from ProjectStewardshipAreaType
+
+Insert Into dbo.ProjectStewardshipAreaType (ProjectStewardshipAreaTypeID, ProjectStewardshipAreaTypeName, ProjectStewardshipAreaTypeDisplayName)
+Values
+(1,'ProjectStewardingOrganizations', 'Project Stewarding Organizations'),
+(2,'TaxonomyBranches', 'Taxonomy Branches')
+GO
