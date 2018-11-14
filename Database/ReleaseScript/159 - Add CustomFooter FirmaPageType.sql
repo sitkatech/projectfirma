127ce46b05dac4b2fb2976c0752df7351ab95125
raynardlee <raynardlee@yahoo.com>
2018-11-14 15:01:35 -0800

    clearing out old release scripts

--- Database/ReleaseScript/159 - Add CustomFooter FirmaPageType.sql
+++
@@ -1,6 +0,0 @@
-insert into dbo.FirmaPageType (FirmaPageTypeID, FirmaPageTypeName, FirmaPageTypeDisplayName ,FirmaPageRenderTypeID)
- values (51, 'CustomFooter', 'Custom Footer', 1)
- 
-
-insert into dbo.FirmaPage(TenantID, FirmaPageTypeID, FirmaPageContent)
-select TenantID, 51 as FirmaPageTypeID, '' as FirmaPageContent from Tenant
--- Database/ReleaseScript/164 - EnableAccomplishmentsDashboard.sql
+++
@@ -1,9 +0,0 @@
-Alter Table dbo.TenantAttribute
-Add EnableAccomplishmentsDashboard bit null
-go
-
-Update dbo.TenantAttribute
-set EnableAccomplishmentsDashboard = 1
-
-Alter Table dbo.TenantAttribute
-Alter Column EnableAccomplishmentsDashboard bit not null
