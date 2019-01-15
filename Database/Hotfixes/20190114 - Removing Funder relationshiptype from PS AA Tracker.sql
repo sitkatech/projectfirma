907297122e55e62fb6ca674032fbe29239422902
raynardlee <raynardlee@yahoo.com>
2019-01-15 10:37:56 -0800

    psp - removed funder relationship type

---
+++ Database/Hotfixes/20190114 - Removing Funder relationshiptype from PS AA Tracker.sql
@@ -0,0 +1,15 @@
+delete
+from dbo.ProjectOrganization
+where RelationshipTypeID = 37 and TenantID = 11
+
+delete
+from dbo.ProjectOrganizationUpdate
+where RelationshipTypeID = 37 and TenantID = 11
+
+delete
+from dbo.OrganizationTypeRelationshipType
+where RelationshipTypeID = 37 and TenantID = 11
+
+delete
+from dbo.RelationshipType
+where RelationshipTypeID = 37 and TenantID = 11
