29e5d574e30721d6b69b1ec2b111dc90d1e9284f
raynardlee <raynardlee@yahoo.com>
2019-01-21 14:20:51 -0800

    [delta/#184] Source ProjectFirma PMs from external source forgot to check in release script

---
+++ Database/ReleaseScript/0209 - removing unused tables.sql
@@ -0,0 +1,15 @@
+drop table dbo.MappedRegion
+drop table dbo.MonitoringProgramPartner
+drop table dbo.MonitoringProgramDocument
+drop table dbo.PerformanceMeasureMonitoringProgram
+drop table dbo.MonitoringProgram
+
+drop table dbo.SnapshotOrganizationTypeExpenditure
+drop table dbo.SnapshotPerformanceMeasureSubcategoryOption
+drop table dbo.SnapshotPerformanceMeasure
+drop table dbo.SnapshotProject
+drop table dbo.SnapshotProjectType
+drop table dbo.Snapshot
+
+
+delete from dbo.FirmaPage where FirmaPageTypeID in (44)
