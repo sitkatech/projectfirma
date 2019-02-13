20fd2d6f0128f5f4859c1e70ae98268686f0bc1d
raynardlee <raynardlee@yahoo.com>
2019-02-12 17:08:40 -0800

    now passing SortOrder in API call

--- Source/ProjectFirmaModels/Models/PerformanceMeasureSubcategoryOptionFromProjectFirma.cs
+++ Source/ProjectFirmaModels/Models/PerformanceMeasureSubcategoryOptionFromProjectFirma.cs
@@ -11,12 +11,15 @@
         public string PerformanceMeasureSubcategoryOptionName { get; set; }
         public string PerformanceMeasureSubcategoryName { get; set; }
 
+        public int? SortOrder { get; set; }
+
         public PerformanceMeasureSubcategoryOptionFromProjectFirma(IPerformanceMeasureValueSubcategoryOption performanceMeasureValueSubcategoryOption)
         {
             PerformanceMeasureSubcategoryOptionID = performanceMeasureValueSubcategoryOption.PerformanceMeasureSubcategoryOptionID;
             PerformanceMeasureSubcategoryID = performanceMeasureValueSubcategoryOption.PerformanceMeasureSubcategoryID;
             PerformanceMeasureSubcategoryOptionName = performanceMeasureValueSubcategoryOption.GetPerformanceMeasureSubcategoryOptionName();
             PerformanceMeasureSubcategoryName = performanceMeasureValueSubcategoryOption.PerformanceMeasureSubcategory.PerformanceMeasureSubcategoryDisplayName;
+            SortOrder = performanceMeasureValueSubcategoryOption.PerformanceMeasureSubcategoryOption.SortOrder;
         }
     }
 }
