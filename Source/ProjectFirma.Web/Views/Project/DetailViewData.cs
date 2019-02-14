27f8cc36cd41215ae330d2e5611a8f913cfd18c6
raynardlee <raynardlee@yahoo.com>
2019-02-14 00:38:33 -0800

    project custom attribute values should now be showing; weird razor syntax causing it to not show

--- Source/ProjectFirma.Web/Views/Project/DetailViewData.cs
+++ Source/ProjectFirma.Web/Views/Project/DetailViewData.cs
@@ -237,7 +237,7 @@ namespace ProjectFirma.Web.Views.Project
             }
 
             
-            if (ProjectFirmaModels.Models.ProjectModelExtensions.GetLatestNotApprovedUpdateBatch(project) != null)
+            if (ProjectModelExtensions.GetLatestNotApprovedUpdateBatch(project) != null)
             {
                 if (userHasProjectAdminPermissions || currentPerson.CanStewardProject(project))
                 {
