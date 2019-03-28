bf8d8c85650d182e4ae5aca48297577a1c9824f4
tomkamin <tom.kamin@sitkatech.com>
2019-03-27 17:38:51 -0700

    bug -adding unit test to check for missing firma page types in the firma pages table per tenant

---
+++ Source/ProjectFirma.Web/Models/FirmaPageTypeTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using ProjectFirma.Web.Common;
+using ProjectFirmaModels.Models;
+
+namespace ProjectFirma.Web.Models
+{
+    [TestFixture]
+    public class FirmaPageTypeTest
+    {
+        [Test]
+        [UseReporter(typeof(DiffReporter))]
+        public void CheckForUncreatedFirmaPageForFirmaPageType()
+        {
+            var allFirmaPageTypes = HttpRequestStorage.DatabaseEntities.FirmaPageTypes.ToList();
+            var allFirmaPages = HttpRequestStorage.DatabaseEntities.FirmaPages.ToList();
+            var allTenants = Tenant.All;
+            string missingPageTypes = string.Empty;
+
+            foreach (var tenant in allTenants)
+            {
+                List<int> allFirmaPageTypeIDPresentInFirmaPages = allFirmaPages.Where(fp => fp.TenantID == tenant.TenantID).Select(fp => fp.FirmaPageTypeID).Distinct().ToList();
+                foreach (var firmaPageType in allFirmaPageTypes)
+                {
+                    var pageTypeIsPresent = allFirmaPageTypeIDPresentInFirmaPages.Contains(firmaPageType.FirmaPageTypeID);
+                    if (!pageTypeIsPresent)
+                    {
+                        missingPageTypes += $"Could Not find Firma Page Type '{firmaPageType.FirmaPageTypeName}'({firmaPageType.FirmaPageTypeID}) in Firma Pages for Tenant {tenant.TenantName}({tenant.TenantID})\n\r";
+                    }
+
+                }
+            }
+            Approvals.Verify(missingPageTypes);
+        }
+    }
+}
