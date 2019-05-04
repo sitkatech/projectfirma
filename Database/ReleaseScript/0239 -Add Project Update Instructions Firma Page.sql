0a92da98ebdc43fe483f0e2569b7951adbd7a30b
Liz Christeleit <liz.christeleit@sitkatech.com>
2019-05-04 08:21:49 -0700

    Minor text change to the instructions

--- Database/ReleaseScript/0239 -Add Project Update Instructions Firma Page.sql
+++ Database/ReleaseScript/0239 -Add Project Update Instructions Firma Page.sql
@@ -7,7 +7,7 @@ go
 
 
 insert into dbo.FirmaPage(TenantID, FirmaPageTypeID, FirmaPageContent)
-select TenantID, 56, '<p>Use this Project Update workflow to submit Project updates about the Project''s location, accomplishments, financial information, photos and more. Projects in the Planning/Design, Implementation, or Post-Implementation stage can be updated using this workflow.</p>
+select TenantID, 56, '<p>Use this Project Update workflow to submit changes or additions to the Project''s location, accomplishments, financial information, photos and more. Projects in the Planning/Design, Implementation, or Post-Implementation stage can be updated using this workflow.</p>
 <p>The various aspects of a Project that are available for update can be seen on the left. Several of these sections are optional, but others are required:</p>
 <ul>
     <li><strong>Basics:</strong> <i>You must enter the Project''s description, status, planning / design and implementation start dates</i></li>
@@ -19,7 +19,7 @@ where TenantID not in (8, 11)
 
 insert into dbo.FirmaPage(TenantID, FirmaPageTypeID, FirmaPageContent)
 values 
-(8, 56, '<p>Use this Project Update workflow to submit Project updates about the Project''s location, accomplishments, financial information, photos and more. Projects in the Planning/Design, Implementation, or Post-Implementation stage can be updated using this workflow.</p>
+(8, 56, '<p>Use this Project Update workflow to submit changes or additions to the Project''s location, accomplishments, financial information, photos and more. Projects in the Planning/Design, Implementation, or Post-Implementation stage can be updated using this workflow.</p>
 <p>The various aspects of a Project that are available for update can be seen on the left. Several of these sections are optional, but others are required:</p>
 <ul>
     <li><strong>Basics:</strong> <i>You must enter the Project''s description, status, planning / design and implementation start dates</i></li>
@@ -27,7 +27,7 @@ values
     <li><strong>Accomplishments:</strong> <i>You must enter an accomplishment value for at least one Indicator per year (or indicate you have no accomplishments to report)</i></li>
     <li><strong>Expenditures:</strong> <i>You must enter at least one Expenditure per year (if you spent nothing, enter a $0 expenditure)</i></li>
 </ul>'),
-(11, 56, '<p>Use this Near Term Action Update workflow to submit Near Term Action updates about the Near Term Action''s location, accomplishments, financial information, photos and more. Near Term Actions in the Planning/Design, Implementation, or Post-Implementation stage can be updated using this workflow.</p>
+(11, 56, '<p>Use this Near Term Action Update workflow to submit changes or additions to the Near Term Action''s location, accomplishments, financial information, photos and more. Near Term Actions in the Planning/Design, Implementation, or Post-Implementation stage can be updated using this workflow.</p>
 <p>The various aspects of a Near Term Action that are available for update can be seen on the left. Several of these sections are optional, but others are required:</p>
 <ul>
     <li><strong>Basics:</strong> <i>You must enter the Near Term Action''s description, status, planning / design and implementation start dates</i></li>
