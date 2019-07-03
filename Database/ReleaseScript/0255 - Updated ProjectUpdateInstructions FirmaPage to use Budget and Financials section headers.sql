9be938194484baeaf7a3a1ef7533d58843bbff2d
bGrass <brazen8s@gmail.com>
2019-07-02 21:39:50 -0700

    Add missing dbo. to table names in release script

--- Database/ReleaseScript/0255 - Updated ProjectUpdateInstructions FirmaPage to use Budget and Financials section headers.sql
+++ Database/ReleaseScript/0255 - Updated ProjectUpdateInstructions FirmaPage to use Budget and Financials section headers.sql
@@ -1,6 +1,6 @@
-Update FirmaPage set FirmaPageContent = REPLACE(FirmaPageContent COLLATE Latin1_General_BIN, 'Expenditures', 'Financials')
+Update dbo.FirmaPage set FirmaPageContent = REPLACE(FirmaPageContent COLLATE Latin1_General_BIN, 'Expenditures', 'Financials')
 where FirmaPageTypeID = 56 and TenantID != 11
-Update FirmaPage set FirmaPageContent = REPLACE(FirmaPageContent, 'Expected Funding', 'Budget')
+Update dbo.FirmaPage set FirmaPageContent = REPLACE(FirmaPageContent, 'Expected Funding', 'Budget')
 where TenantID = 11 and FirmaPageTypeID = 56
-Update FirmaPage set FirmaPageContent = REPLACE(FirmaPageContent COLLATE Latin1_General_BIN, 'Expenditures (', 'Financials (')
+Update dbo.FirmaPage set FirmaPageContent = REPLACE(FirmaPageContent COLLATE Latin1_General_BIN, 'Expenditures (', 'Financials (')
 where FirmaPageTypeID = 56 and TenantID != 11
