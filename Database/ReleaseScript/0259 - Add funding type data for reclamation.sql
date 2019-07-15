204309738437a374f86e02deda94106a1d9223b1
tomkamin <tom.kamin@sitkatech.com>
2019-07-15 09:30:36 -0700

    [projectfirma/#1703] bug: Add new project crashes removing release script no longer needed after merging

--- Database/ReleaseScript/0259 - Add funding type data for reclamation.sql
+++
@@ -1,8 +0,0 @@
-insert into dbo.FundingTypeData(TenantID, FundingTypeID, FundingTypeDisplayName, FundingTypeShortName, SortOrder)
-SELECT 12 as TenantID
-	  ,FundingTypeID
-	  ,FundingTypeDisplayName
-	  ,FundingTypeShortName
-	  ,SortOrder
-FROM dbo.FundingTypeData
-where TenantID = 4
