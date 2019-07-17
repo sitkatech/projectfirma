11b9cc94d01569c792678193a961468f0197b74a
StewLGSitka <stewart@sitkatech.com>
2019-07-17 13:28:39 -0700

    ProjectFirma: Fixing FirmaPageType issue left by some dev as broken test.

---
+++ Database/ReleaseScript/0268 - ManageFundingSourceCustomAttributesTypeList for Idaho Soil.sql
@@ -0,0 +1,17 @@
+--insert into dbo.FirmaPage(FirmaPageTypeID, TenantID) values(52, 12);
+--insert into dbo.FirmaPage(FirmaPageTypeID, TenantID) values(53, 12);
+--insert into dbo.FirmaPage(FirmaPageTypeID, TenantID) values(54, 12);
+--insert into dbo.FirmaPage(FirmaPageTypeID, TenantID) values(55, 12);
+
+--select * from dbo.FirmaPage
+
+--select * from dbo.FirmaPageType
+
+-- Could Not find Firma Page Type 'ManageFundingSourceCustomAttributeTypeInstructions'(60) in Firma Pages for Tenant IdahoAssociatonOfSoilConservationDistricts(9)
+-- Could Not find Firma Page Type 'ManageFundingSourceCustomAttributeTypesList'(61) in Firma Pages for Tenant IdahoAssociatonOfSoilConservationDistricts(9)
+insert into dbo.FirmaPage(FirmaPageTypeID, TenantID) values(60, 9);
+insert into dbo.FirmaPage(FirmaPageTypeID, TenantID) values(61, 9);
+
+
+
+--select * from Tenant
