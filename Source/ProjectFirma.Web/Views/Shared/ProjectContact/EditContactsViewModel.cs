2a80f718a06b6a8b94548705fc1f8de6c47457de
tomkamin <tom.kamin@sitkatech.com>
2019-07-22 18:31:00 -0700

    [projectfirma/#1654] Create Project Contacts starting to build out editors for adding contacts to projects

---
+++ Source/ProjectFirma.Web/Views/Shared/ProjectContact/EditContactsViewModel.cs
@@ -0,0 +1,135 @@
+﻿/*-----------------------------------------------------------------------
+<copyright file="EditContactsViewModel.cs" company="Tahoe Regional Planning Agency and Sitka Technology Group">
+Copyright (c) Tahoe Regional Planning Agency and Sitka Technology Group. All rights reserved.
+<author>Sitka Technology Group</author>
+</copyright>
+
+<license>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License <http://www.gnu.org/licenses/> for more details.
+
+Source code is available upon request via <support@sitkatech.com>.
+</license>
+-----------------------------------------------------------------------*/
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using LtInfo.Common.Models;
+using ProjectFirma.Web.Common;
+using ProjectFirma.Web.Models;
+using ProjectFirmaModels;
+using ProjectFirmaModels.Models;
+
+namespace ProjectFirma.Web.Views.Shared.ProjectContact
+{
+    public class EditContactsViewModel : FormViewModel, IValidatableObject
+    {
+        [FieldDefinitionDisplay(FieldDefinitionEnum.ProjectPrimaryContact)]
+        public int? PrimaryContactPersonID { get; set; }
+        public List<ProjectOrganizationSimple> ProjectOrganizationSimples { get; set; }
+
+        /// <summary>
+        /// Needed by Model Binder
+        /// </summary>
+        public EditContactsViewModel()
+        {
+        }
+
+        public EditContactsViewModel(ProjectFirmaModels.Models.Project project, List<ProjectFirmaModels.Models.ProjectOrganization> projectOrganizations,
+            Person currentPerson)
+        {           
+            PrimaryContactPersonID = project.PrimaryContactPersonID;
+            
+            ProjectOrganizationSimples = projectOrganizations.Select(x => new ProjectOrganizationSimple(x)).ToList();
+
+            // If the current person belongs to a primary contact organization, and the current project has no primary contact organization set, prepopulate.
+            if (currentPerson != null && currentPerson.Organization.CanBeAPrimaryContactOrganization())
+            {
+                var primaryContactOrganizationRelationshipTypeIDs = HttpRequestStorage.DatabaseEntities.OrganizationRelationshipTypes
+                    .Where(x => x.IsPrimaryContact).Select(x => x.OrganizationRelationshipTypeID).ToList();
+                if (!projectOrganizations.Any(x => primaryContactOrganizationRelationshipTypeIDs.Contains(x.OrganizationRelationshipTypeID)))
+                {
+                    ProjectOrganizationSimples.AddRange(primaryContactOrganizationRelationshipTypeIDs.Select(x =>
+                        new ProjectOrganizationSimple(currentPerson.OrganizationID, x)));
+                }
+            }
+        }
+
+        public void UpdateModel(ProjectFirmaModels.Models.Project project, ICollection<ProjectFirmaModels.Models.ProjectOrganization> allProjectOrganizations)
+        {
+            project.PrimaryContactPersonID = PrimaryContactPersonID;
+
+            var projectOrganizationsUpdated = ProjectOrganizationSimples.Where(x => ModelObjectHelpers.IsRealPrimaryKeyValue(x.OrganizationID)).Select(x =>
+                new ProjectFirmaModels.Models.ProjectOrganization(project.ProjectID, x.OrganizationID, x.OrganizationRelationshipTypeID)).ToList();
+
+            project.ProjectOrganizations.Merge(projectOrganizationsUpdated,
+                allProjectOrganizations,
+                (x, y) => x.ProjectID == y.ProjectID && x.OrganizationID == y.OrganizationID && x.OrganizationRelationshipTypeID == y.OrganizationRelationshipTypeID, HttpRequestStorage.DatabaseEntities);
+        }
+
+        public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            return GetValidationResults();
+        }
+
+        public IEnumerable<ValidationResult> GetValidationResults()
+        {
+            var errors = new List<ValidationResult>();
+
+            if (ProjectOrganizationSimples == null)
+            {
+                ProjectOrganizationSimples = new List<ProjectOrganizationSimple>();
+            }
+
+            if (ProjectOrganizationSimples.GroupBy(x => new { RelationshipTypeID = x.OrganizationRelationshipTypeID, x.OrganizationID }).Any(x => x.Count() > 1))
+            {
+                errors.Add(new ValidationResult($"Cannot have the same relationship type listed for the same {FieldDefinitionEnum.Organization.ToType().GetFieldDefinitionLabel()} multiple times."));
+            }
+            
+            var relationshipTypeThatMustBeRelatedOnceToAProject = HttpRequestStorage.DatabaseEntities.OrganizationRelationshipTypes.Where(x => x.CanOnlyBeRelatedOnceToAProject).ToList();
+
+            var projectOrganizationsGroupedByRelationshipTypeID =
+                ProjectOrganizationSimples.GroupBy(x => x.OrganizationRelationshipTypeID).ToList();
+
+            errors.AddRange(relationshipTypeThatMustBeRelatedOnceToAProject
+                .Where(rt => projectOrganizationsGroupedByRelationshipTypeID.Count(po => po.Key == rt.OrganizationRelationshipTypeID) > 1)
+                .Select(relationshipType => new ValidationResult(
+                    $"Cannot have more than one {FieldDefinitionEnum.Organization.ToType().GetFieldDefinitionLabel()} with a {FieldDefinitionEnum.ProjectRelationshipType.ToType().GetFieldDefinitionLabel()} set to \"{relationshipType.OrganizationRelationshipTypeName}\".")));
+
+            errors.AddRange(relationshipTypeThatMustBeRelatedOnceToAProject
+                .Where(rt => projectOrganizationsGroupedByRelationshipTypeID.Count(po => po.Key == rt.OrganizationRelationshipTypeID) == 0)
+                .Select(relationshipType => new ValidationResult(
+                    $"Must have one {FieldDefinitionEnum.Organization.ToType().GetFieldDefinitionLabel()} with a {FieldDefinitionEnum.ProjectRelationshipType.ToType().GetFieldDefinitionLabel()} set to \"{relationshipType.OrganizationRelationshipTypeName}\".")));
+
+            var allValidRelationshipTypes = ProjectOrganizationSimples.All(x =>
+            {
+                var organization = HttpRequestStorage.DatabaseEntities.Organizations.GetOrganization(x.OrganizationID);
+                var organizationType = organization.OrganizationType;
+
+                if (organizationType != null)
+                {
+                    var organizationTypeOrganizationRelationshipTypeIDs =
+                        organizationType.OrganizationTypeOrganizationRelationshipTypes.Select(y => y.OrganizationRelationshipTypeID);
+
+                    return organizationTypeOrganizationRelationshipTypeIDs.Contains(x.OrganizationRelationshipTypeID);
+                }
+                return false;
+            });
+
+            if (!allValidRelationshipTypes)
+            {
+                errors.Add(new ValidationResult("One or more relationship types are invalid."));
+            }
+
+            return errors;
+        }
+    }
+}
---
+++ Source/ProjectFirma.Web/Views/Shared/ProjectContact/ProjectContactController.js
@@ -0,0 +1,256 @@
+﻿/*-----------------------------------------------------------------------
+<copyright file="ProjectOrganizationController.js" company="Tahoe Regional Planning Agency and Sitka Technology Group">
+Copyright (c) Tahoe Regional Planning Agency and Sitka Technology Group. All rights reserved.
+<author>Sitka Technology Group</author>
+</copyright>
+
+<license>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License <http://www.gnu.org/licenses/> for more details.
+
+Source code is available upon request via <support@sitkatech.com>.
+</license>
+-----------------------------------------------------------------------*/
+angular.module("ProjectFirmaApp").controller("ProjectContactController", function($scope,
+    angularModelAndViewData) {
+    $scope.OrganizationToAdd = null;
+
+    $scope.$watch(function() {
+        jQuery(".selectpicker").selectpicker("refresh");
+
+        // so that unsavedChanges.js knows to check if the form has changed.
+        jQuery("form").trigger("input");
+    });
+
+    $scope.getAvailableOrganizationsForRelationshipType = function (relationshipType, comingFrom) {
+        //debugger;
+        if (relationshipType == null) {
+            //debugger;
+            return null;
+        }
+
+        var organizationsForRelationshipType = _.filter($scope.AngularViewData.AllOrganizations,
+            function(organization) {
+                return $scope.organizationIsValidForRelationshipType(organization, relationshipType);
+            });
+        if (relationshipType.OrganizationRelationshipTypeCanOnlyBeRelatedOnceToAProject) {
+            return organizationsForRelationshipType;
+        } else {
+            var usedOrganizations = _.filter($scope.AngularModel.ProjectOrganizationSimples,
+                function(f) {
+                    return f.OrganizationRelationshipTypeID == relationshipType.OrganizationRelationshipTypeID;
+                });
+            var usedOrganizationIDs = _.map(usedOrganizations,
+                function (f) {
+                    return f.OrganizationID;
+                });
+
+            var filteredList = _.filter(organizationsForRelationshipType,
+                function (f) {
+                    return !_.includes(usedOrganizationIDs, f.OrganizationID);
+                });
+
+            return filteredList;
+        }
+    };
+
+    $scope.organizationIsValidForRelationshipType = function(organization, relationshipType) {
+        var validRelationshipTypeIDs = _.map($scope.validRelationshipTypes(organization.OrganizationID),
+            function(rt) {
+                return rt.OrganizationRelationshipTypeID;
+            });
+        return _.includes(validRelationshipTypeIDs, relationshipType.OrganizationRelationshipTypeID);
+    };
+
+    $scope.chosenOrganizationsForRelationshipType = function(relationshipTypeID) {
+        var chosenOrganizationSimples = _.filter($scope.AngularModel.ProjectOrganizationSimples,
+            function(s) {
+                return s.OrganizationRelationshipTypeID == relationshipTypeID;
+            });
+
+        var organizations = _.map(chosenOrganizationSimples,
+            function(s) {
+                var organization =
+                    Sitka.Methods.findElementInJsonArray($scope.AngularViewData.AllOrganizations,
+                        "OrganizationID",
+                        s.OrganizationID);
+                return organization;
+            });
+        return organizations;
+    };
+
+    $scope.canSetOrganizationFromProjectLocation = function(relationshipType) {
+        if (!relationshipType.OrganizationRelationshipTypeCanOnlyBeRelatedOnceToAProject ||
+            !relationshipType.OrganizationRelationshipTypeHasOrganizationsWithSpatialBoundary) {
+            return false;
+        }
+
+        if ($scope.AngularViewData.OrganizationContainingProjectSimpleLocation[relationshipType.OrganizationRelationshipTypeID] ===
+            null) {
+            return false;
+        }
+
+        return true;
+    };
+
+    $scope.setProjectOrganizationSimpleFromProjectLocation = function(relationshipType) {
+        if (!$scope.canSetOrganizationFromProjectLocation(relationshipType)) {
+            return;
+        }
+
+        var organizationID = Number(
+            $scope.AngularViewData.OrganizationContainingProjectSimpleLocation[relationshipType.OrganizationRelationshipTypeID]
+            .OrganizationID);
+
+        $scope.selectionChanged(organizationID, relationshipType);
+    };
+
+    $scope.selectedOrgDoesNotMatchSpatialOrg = function (relationshipType) {
+
+        if (!$scope.canSetOrganizationFromProjectLocation(relationshipType)) {
+            return false;
+        }
+
+        var spatialOrganizationID = Number(
+            $scope.AngularViewData.OrganizationContainingProjectSimpleLocation[relationshipType.OrganizationRelationshipTypeID].OrganizationID);
+
+        var projectOrganizationSimple =
+            Sitka.Methods.findElementInJsonArray($scope.AngularModel.ProjectOrganizationSimples,
+                "RelationshipTypeID",
+                relationshipType.OrganizationRelationshipTypeID);
+
+        return spatialOrganizationID !== projectOrganizationSimple.OrganizationID;
+    };
+
+    $scope.addProjectOrganizationSimple = function(organizationID, relationshipTypeID) {
+        $scope.AngularModel.ProjectOrganizationSimples.push({
+            OrganizationID: Number(organizationID),
+            OrganizationRelationshipTypeID: relationshipTypeID
+        });
+        $scope.resetSelectedOrganizationID(relationshipTypeID);
+    };
+
+    $scope.removeProjectOrganizationSimple = function(organizationID, relationshipTypeID) {
+        _.remove($scope.AngularModel.ProjectOrganizationSimples,
+            function(pos) {
+                return pos.OrganizationID == organizationID && pos.OrganizationRelationshipTypeID == relationshipTypeID;
+            });
+    };
+
+    $scope.selectionChanged = function(organizationID, relationshipType) {
+        // changing the dropdown selection for a one-and-only-one relationship type should update the model
+        if (relationshipType.OrganizationRelationshipTypeCanOnlyBeRelatedOnceToAProject) {
+            // if there's already a projectOrganizationSimple for this relationship type, just change the OrganizationID
+            var projectOrganizationSimple =
+                Sitka.Methods.findElementInJsonArray($scope.AngularModel.ProjectOrganizationSimples,
+                    "RelationshipTypeID",
+                    relationshipType.OrganizationRelationshipTypeID);
+
+            if (projectOrganizationSimple != null) {
+                projectOrganizationSimple.OrganizationID = Number(organizationID);
+            } else {
+                $scope.AngularModel.ProjectOrganizationSimples.push({
+                    OrganizationID: Number(organizationID),
+                    OrganizationRelationshipTypeID: relationshipType.OrganizationRelationshipTypeID
+                });
+            }
+        } // but nothing should happen if it's a many-or-none relationship type
+    };
+
+    $scope.resetSelectedOrganizationID = function(relationshipTypeID) {
+        $scope.selectedOrganizationID[relationshipTypeID] = "";
+    };
+
+    $scope.isOptionSelected = function(organization, relationshipType) {
+        if (!relationshipType.OrganizationRelationshipTypeCanOnlyBeRelatedOnceToAProject) {
+            return false;
+        }
+        return _.any($scope.AngularModel.ProjectOrganizationSimples,
+            function(pos) {
+                return pos.OrganizationID == organization.OrganizationID &&
+                    pos.OrganizationRelationshipTypeID == relationshipType.OrganizationRelationshipTypeID;
+            });
+    };
+
+    $scope.dropdownDefaultOption = function (relationshipType) {
+        if (relationshipType == null) {
+            return "Select One";
+        }
+
+        if (relationshipType.OrganizationRelationshipTypeCanOnlyBeRelatedOnceToAProject) {
+            return "Select the " + relationshipType.OrganizationRelationshipTypeName;
+        } else {
+            return "Add a " + relationshipType.OrganizationRelationshipTypeName;
+        }
+    };
+
+    $scope.validRelationshipTypes = function (organizationID) {
+        //debugger;
+        var organization =
+            Sitka.Methods.findElementInJsonArray($scope.AngularViewData.AllOrganizations,
+                "OrganizationID",
+                organizationID);
+
+        var valid = organization == null ? [] : organization.ValidOrganizationRelationshipTypeSimples;
+        return valid;
+    };
+
+    $scope.getSelectedPrimaryContactOrganization = function (relationshipType) {
+        if (relationshipType === null) {
+            return null;
+        }
+
+        var selectedPrimaryContactOrganizationID =
+            Sitka.Methods.findElementInJsonArray($scope.AngularModel.ProjectOrganizationSimples,
+                "RelationshipTypeID",
+                relationshipType.OrganizationRelationshipTypeID).OrganizationID;
+
+        var selectedPrimaryContactOrganization =
+            Sitka.Methods.findElementInJsonArray($scope.AngularViewData.AllOrganizations,
+                "OrganizationID",
+                selectedPrimaryContactOrganizationID);
+
+        return selectedPrimaryContactOrganization;
+    }
+
+    $scope.primaryContactOrganizationHasNoPrimaryContact = function(relationshipType) {
+        return $scope.getSelectedPrimaryContactOrganization(relationshipType).PrimaryContactPersonID == null;
+    }
+
+    $scope.primaryContactOrganization = function (relationshipType) {
+        if (relationshipType != null) {
+            return $scope.getSelectedPrimaryContactOrganization(relationshipType);
+        }
+
+        return null;
+    }
+
+    $scope.primaryContactOrganizationPersonDisplayName = function (relationshipType) {
+        if (relationshipType != null) {
+            return $scope.getSelectedPrimaryContactOrganization(relationshipType).PrimaryContactPersonDisplayName;
+        }
+
+        return "nobody";
+    }
+
+    $scope.isPersonSelected = function (personID) {
+        var primaryContactPersonId = $scope.AngularModel.PrimaryContactPersonID;
+
+        return primaryContactPersonId === personID;
+    };
+
+    $scope.primaryContactPersonChange = function (personID) {
+        $scope.AngularModel.PrimaryContactPersonID = personID === "null" ? null : parseInt(personID);
+    }
+
+    $scope.AngularModel = angularModelAndViewData.AngularModel;
+    $scope.AngularViewData = angularModelAndViewData.AngularViewData;
+    $scope.selectedOrganizationID = {};
+});
