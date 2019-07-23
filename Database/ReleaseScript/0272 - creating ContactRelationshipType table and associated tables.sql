aecbbaad025ded49637396eb1bb87cfaaf4aac99
tomkamin <tom.kamin@sitkatech.com>
2019-07-23 12:21:33 -0700

    [projectfirma/#1654] Create Project Contacts removing ContactType table from release script

--- Database/ReleaseScript/0272 - creating ContactRelationshipType table and associated tables.sql
+++ Database/ReleaseScript/0272 - creating ContactRelationshipType table and associated tables.sql
@@ -32,96 +32,96 @@ GO
 
 
 --Create ContactType table
-CREATE TABLE [dbo].[ContactType](
-	[ContactTypeID] [int] IDENTITY(1,1) NOT NULL,
-	[TenantID] [int] NOT NULL,
-	[ContactTypeName] [varchar](200) NOT NULL,
-	[ContactTypeAbbreviation] [varchar](100) NOT NULL,
-	[IsDefaultContactType] [bit] NOT NULL,
- CONSTRAINT [PK_ContactType_ContactTypeID] PRIMARY KEY CLUSTERED 
-(
-	[ContactTypeID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
- CONSTRAINT [AK_ContactType_ContactTypeID_TenantID] UNIQUE NONCLUSTERED 
-(
-	[ContactTypeID] ASC,
-	[TenantID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
- CONSTRAINT [AK_ContactType_ContactTypeName_TenantID] UNIQUE NONCLUSTERED 
-(
-	[ContactTypeName] ASC,
-	[TenantID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+--CREATE TABLE [dbo].[ContactType](
+--	[ContactTypeID] [int] IDENTITY(1,1) NOT NULL,
+--	[TenantID] [int] NOT NULL,
+--	[ContactTypeName] [varchar](200) NOT NULL,
+--	[ContactTypeAbbreviation] [varchar](100) NOT NULL,
+--	[IsDefaultContactType] [bit] NOT NULL,
+-- CONSTRAINT [PK_ContactType_ContactTypeID] PRIMARY KEY CLUSTERED 
+--(
+--	[ContactTypeID] ASC
+--)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+-- CONSTRAINT [AK_ContactType_ContactTypeID_TenantID] UNIQUE NONCLUSTERED 
+--(
+--	[ContactTypeID] ASC,
+--	[TenantID] ASC
+--)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+-- CONSTRAINT [AK_ContactType_ContactTypeName_TenantID] UNIQUE NONCLUSTERED 
+--(
+--	[ContactTypeName] ASC,
+--	[TenantID] ASC
+--)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+--) ON [PRIMARY]
+--GO
 
-ALTER TABLE [dbo].[ContactType]  WITH CHECK ADD  CONSTRAINT [FK_ContactType_Tenant_TenantID] FOREIGN KEY([TenantID])
-REFERENCES [dbo].[Tenant] ([TenantID])
-GO
+--ALTER TABLE [dbo].[ContactType]  WITH CHECK ADD  CONSTRAINT [FK_ContactType_Tenant_TenantID] FOREIGN KEY([TenantID])
+--REFERENCES [dbo].[Tenant] ([TenantID])
+--GO
 
-ALTER TABLE [dbo].[ContactType] CHECK CONSTRAINT [FK_ContactType_Tenant_TenantID]
-GO
+--ALTER TABLE [dbo].[ContactType] CHECK CONSTRAINT [FK_ContactType_Tenant_TenantID]
+--GO
 
 
 
 --create joining table ContactTypeContactRelationshipType
-CREATE TABLE [dbo].[ContactTypeContactRelationshipType](
-	[ContactTypeContactRelationshipTypeID] [int] IDENTITY(1,1) NOT NULL,
-	[TenantID] [int] NOT NULL,
-	[ContactTypeID] [int] NOT NULL,
-	[ContactRelationshipTypeID] [int] NOT NULL,
- CONSTRAINT [PK_ContactTypeContactRelationshipType_ContactTypeContactRelationshipTypeID] PRIMARY KEY CLUSTERED 
-(
-	[ContactTypeContactRelationshipTypeID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
- CONSTRAINT [AK_ContactTypeContactRelationshipType_ContactTypeID_ContactRelationshipTypeID_TenantID] UNIQUE NONCLUSTERED 
-(
-	[ContactTypeID] ASC,
-	[ContactRelationshipTypeID] ASC,
-	[TenantID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
- CONSTRAINT [AK_ContactTypeContactRelationshipType_ContactTypeContactRelationshipTypeID_TenantID] UNIQUE NONCLUSTERED 
-(
-	[ContactTypeContactRelationshipTypeID] ASC,
-	[TenantID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+--CREATE TABLE [dbo].[ContactTypeContactRelationshipType](
+--	[ContactTypeContactRelationshipTypeID] [int] IDENTITY(1,1) NOT NULL,
+--	[TenantID] [int] NOT NULL,
+--	[ContactTypeID] [int] NOT NULL,
+--	[ContactRelationshipTypeID] [int] NOT NULL,
+-- CONSTRAINT [PK_ContactTypeContactRelationshipType_ContactTypeContactRelationshipTypeID] PRIMARY KEY CLUSTERED 
+--(
+--	[ContactTypeContactRelationshipTypeID] ASC
+--)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+-- CONSTRAINT [AK_ContactTypeContactRelationshipType_ContactTypeID_ContactRelationshipTypeID_TenantID] UNIQUE NONCLUSTERED 
+--(
+--	[ContactTypeID] ASC,
+--	[ContactRelationshipTypeID] ASC,
+--	[TenantID] ASC
+--)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+-- CONSTRAINT [AK_ContactTypeContactRelationshipType_ContactTypeContactRelationshipTypeID_TenantID] UNIQUE NONCLUSTERED 
+--(
+--	[ContactTypeContactRelationshipTypeID] ASC,
+--	[TenantID] ASC
+--)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+--) ON [PRIMARY]
+--GO
 
-ALTER TABLE [dbo].[ContactTypeContactRelationshipType]  WITH CHECK ADD  CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactRelationshipType_ContactRelationshipTypeID] FOREIGN KEY([ContactRelationshipTypeID])
-REFERENCES [dbo].[ContactRelationshipType] ([ContactRelationshipTypeID])
-GO
+--ALTER TABLE [dbo].[ContactTypeContactRelationshipType]  WITH CHECK ADD  CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactRelationshipType_ContactRelationshipTypeID] FOREIGN KEY([ContactRelationshipTypeID])
+--REFERENCES [dbo].[ContactRelationshipType] ([ContactRelationshipTypeID])
+--GO
 
-ALTER TABLE [dbo].[ContactTypeContactRelationshipType] CHECK CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactRelationshipType_ContactRelationshipTypeID]
-GO
+--ALTER TABLE [dbo].[ContactTypeContactRelationshipType] CHECK CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactRelationshipType_ContactRelationshipTypeID]
+--GO
 
-ALTER TABLE [dbo].[ContactTypeContactRelationshipType]  WITH CHECK ADD  CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactRelationshipType_ContactRelationshipTypeID_TenantID] FOREIGN KEY([ContactRelationshipTypeID], [TenantID])
-REFERENCES [dbo].[ContactRelationshipType] ([ContactRelationshipTypeID], [TenantID])
-GO
+--ALTER TABLE [dbo].[ContactTypeContactRelationshipType]  WITH CHECK ADD  CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactRelationshipType_ContactRelationshipTypeID_TenantID] FOREIGN KEY([ContactRelationshipTypeID], [TenantID])
+--REFERENCES [dbo].[ContactRelationshipType] ([ContactRelationshipTypeID], [TenantID])
+--GO
 
-ALTER TABLE [dbo].[ContactTypeContactRelationshipType] CHECK CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactRelationshipType_ContactRelationshipTypeID_TenantID]
-GO
+--ALTER TABLE [dbo].[ContactTypeContactRelationshipType] CHECK CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactRelationshipType_ContactRelationshipTypeID_TenantID]
+--GO
 
-ALTER TABLE [dbo].[ContactTypeContactRelationshipType]  WITH CHECK ADD  CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactType_ContactTypeID] FOREIGN KEY([ContactTypeID])
-REFERENCES [dbo].[ContactType] ([ContactTypeID])
-GO
+--ALTER TABLE [dbo].[ContactTypeContactRelationshipType]  WITH CHECK ADD  CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactType_ContactTypeID] FOREIGN KEY([ContactTypeID])
+--REFERENCES [dbo].[ContactType] ([ContactTypeID])
+--GO
 
-ALTER TABLE [dbo].[ContactTypeContactRelationshipType] CHECK CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactType_ContactTypeID]
-GO
+--ALTER TABLE [dbo].[ContactTypeContactRelationshipType] CHECK CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactType_ContactTypeID]
+--GO
 
-ALTER TABLE [dbo].[ContactTypeContactRelationshipType]  WITH CHECK ADD  CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactType_ContactTypeID_TenantID] FOREIGN KEY([ContactTypeID], [TenantID])
-REFERENCES [dbo].[ContactType] ([ContactTypeID], [TenantID])
-GO
+--ALTER TABLE [dbo].[ContactTypeContactRelationshipType]  WITH CHECK ADD  CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactType_ContactTypeID_TenantID] FOREIGN KEY([ContactTypeID], [TenantID])
+--REFERENCES [dbo].[ContactType] ([ContactTypeID], [TenantID])
+--GO
 
-ALTER TABLE [dbo].[ContactTypeContactRelationshipType] CHECK CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactType_ContactTypeID_TenantID]
-GO
+--ALTER TABLE [dbo].[ContactTypeContactRelationshipType] CHECK CONSTRAINT [FK_ContactTypeContactRelationshipType_ContactType_ContactTypeID_TenantID]
+--GO
 
-ALTER TABLE [dbo].[ContactTypeContactRelationshipType]  WITH CHECK ADD  CONSTRAINT [FK_ContactTypeContactRelationshipType_Tenant_TenantID] FOREIGN KEY([TenantID])
-REFERENCES [dbo].[Tenant] ([TenantID])
-GO
+--ALTER TABLE [dbo].[ContactTypeContactRelationshipType]  WITH CHECK ADD  CONSTRAINT [FK_ContactTypeContactRelationshipType_Tenant_TenantID] FOREIGN KEY([TenantID])
+--REFERENCES [dbo].[Tenant] ([TenantID])
+--GO
 
-ALTER TABLE [dbo].[ContactTypeContactRelationshipType] CHECK CONSTRAINT [FK_ContactTypeContactRelationshipType_Tenant_TenantID]
-GO
+--ALTER TABLE [dbo].[ContactTypeContactRelationshipType] CHECK CONSTRAINT [FK_ContactTypeContactRelationshipType_Tenant_TenantID]
+--GO
 
 
 
@@ -266,7 +266,7 @@ GO
 
 
 
---add reference to ContactType on Person table
-alter table dbo.Person
-add ContactTypeID int null constraint FK_Person_ContactType_ContactTypeID foreign key references dbo.ContactType(ContactTypeID);
+----add reference to ContactType on Person table
+--alter table dbo.Person
+--add ContactTypeID int null constraint FK_Person_ContactType_ContactTypeID foreign key references dbo.ContactType(ContactTypeID);
 
