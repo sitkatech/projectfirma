14eb891413fc23fa1a9d2172528fd05f4bf70364
bGrass <brazen8s@gmail.com>
2019-08-07 17:05:36 -0700

    Handle null dollar amounts in budgets when creating ProjectUpdate from Project

--- Source/ProjectFirma.Web/Models/ProjectFundingSourceBudgetUpdateModelExtensions.cs
+++ Source/ProjectFirma.Web/Models/ProjectFundingSourceBudgetUpdateModelExtensions.cs
@@ -15,7 +15,8 @@ namespace ProjectFirma.Web.Models
             {
                 projectUpdateBatch.ProjectFundingSourceBudgetUpdates = project.ProjectFundingSourceBudgets.Select(
                     projectFundingSourceBudget =>
-                        new ProjectFundingSourceBudgetUpdate(projectUpdateBatch, projectFundingSourceBudget.FundingSource, projectFundingSourceBudget.CalendarYear.Value, projectFundingSourceBudget.SecuredAmount.Value, projectFundingSourceBudget.TargetedAmount.Value, projectFundingSourceBudget.CostTypeID)
+                        new ProjectFundingSourceBudgetUpdate(projectUpdateBatch, projectFundingSourceBudget.FundingSource, projectFundingSourceBudget.CalendarYear.Value,
+                            projectFundingSourceBudget.SecuredAmount ?? 0, projectFundingSourceBudget.TargetedAmount ?? 0, projectFundingSourceBudget.CostTypeID)
                 ).ToList();
             }
             else
