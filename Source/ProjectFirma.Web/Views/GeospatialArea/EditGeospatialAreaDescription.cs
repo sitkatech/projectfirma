a67d1d0cf0b8b4d2946f9695ad0a8bc422749283
Shannon Bulloch <shannon.bulloch@sitkatech.com>
2019-08-12 14:11:56 -0700

    [projectfirma/#1605] fixed names of files in doc comments

--- Source/ProjectFirma.Web/Views/GeospatialArea/EditGeospatialAreaDescription.cs
+++ Source/ProjectFirma.Web/Views/GeospatialArea/EditGeospatialAreaDescription.cs
@@ -1,5 +1,5 @@
 ﻿/*-----------------------------------------------------------------------
-<copyright file="Edit.cs" company="Tahoe Regional Planning Agency and Sitka Technology Group">
+<copyright file="EditGeospatialAreaDescription.cs" company="Tahoe Regional Planning Agency and Sitka Technology Group">
 Copyright (c) Tahoe Regional Planning Agency and Sitka Technology Group. All rights reserved.
 <author>Sitka Technology Group</author>
 </copyright>
