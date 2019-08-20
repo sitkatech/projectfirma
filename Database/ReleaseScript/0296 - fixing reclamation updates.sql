536292d5c3b7bfcb4392b75201b5485510ab68c6
tomkamin <tom.kamin@sitkatech.com>
2019-08-20 14:44:13 -0700

    [projectfirma/#1789] Allow Users to Add/Edit Attachments fixing issues with rec projects updated

--- Database/ReleaseScript/0296 - fixing reclamation updates.sql
+++ Database/ReleaseScript/0296 - fixing reclamation updates.sql
@@ -1,14 +1,13 @@
 
-
-select * from dbo.Project
-where TenantID = 12
-
-
 update dbo.ProjectUpdate
 set FundingTypeID = 1
 where TenantID = 12 and FundingTypeID is null
 
-
 update dbo.Project
 set FundingTypeID = 1
 where TenantID = 12 and FundingTypeID is null
+
+update dbo.ProjectFundingSourceBudget
+set CalendarYear = 2019, CostTypeID = 4
+where TenantID = 12 and CostTypeID is null
+
