48458feb308164f62f0d168a7f24ce3e9584f4f8
stewart-gordon-sitka <stewart.gordon@sitkatech.com>
2019-08-29 14:18:32 -0700

    [projectfirma/#1814] Add Contacts and Organizations to Project Update change log Generated files from the previous release script

--- Source/ProjectFirmaModels/Models/Generated/ProjectUpdateBatch.Binding.cs
+++ Source/ProjectFirmaModels/Models/Generated/ProjectUpdateBatch.Binding.cs
@@ -50,7 +50,7 @@ namespace ProjectFirmaModels.Models
         /// <summary>
         /// Constructor for building a new object with MaximalConstructor required fields in preparation for insert into database
         /// </summary>
-        public ProjectUpdateBatch(int projectUpdateBatchID, int projectID, DateTime lastUpdateDate, string performanceMeasureActualYearsExemptionExplanation, int lastUpdatePersonID, string basicsComment, string expendituresComment, string reportedPerformanceMeasuresComment, string locationSimpleComment, string locationDetailedComment, string budgetsComment, int projectUpdateStateID, bool isPhotosUpdated, string basicsDiffLog, string performanceMeasureDiffLog, string expendituresDiffLog, string budgetsDiffLog, string externalLinksDiffLog, string notesDiffLog, string geospatialAreaComment, string expectedFundingComment, string expectedFundingDiffLog, string organizationsComment, string organizationsDiffLog, string noExpendituresToReportExplanation, string expectedPerformanceMeasuresComment, string technicalAssistanceRequestsComment, string contactsComment, string expectedFundingUpdateNote) : this()
+        public ProjectUpdateBatch(int projectUpdateBatchID, int projectID, DateTime lastUpdateDate, string performanceMeasureActualYearsExemptionExplanation, int lastUpdatePersonID, string basicsComment, string expendituresComment, string reportedPerformanceMeasuresComment, string locationSimpleComment, string locationDetailedComment, string budgetsComment, int projectUpdateStateID, bool isPhotosUpdated, string basicsDiffLog, string performanceMeasureDiffLog, string expendituresDiffLog, string budgetsDiffLog, string externalLinksDiffLog, string notesDiffLog, string geospatialAreaComment, string expectedFundingComment, string expectedFundingDiffLog, string organizationsComment, string organizationsDiffLog, string noExpendituresToReportExplanation, string expectedPerformanceMeasuresComment, string technicalAssistanceRequestsComment, string contactsComment, string expectedFundingUpdateNote, string contactsDiffLog) : this()
         {
             this.ProjectUpdateBatchID = projectUpdateBatchID;
             this.ProjectID = projectID;
@@ -81,6 +81,7 @@ namespace ProjectFirmaModels.Models
             this.TechnicalAssistanceRequestsComment = technicalAssistanceRequestsComment;
             this.ContactsComment = contactsComment;
             this.ExpectedFundingUpdateNote = expectedFundingUpdateNote;
+            this.ContactsDiffLog = contactsDiffLog;
         }
 
         /// <summary>
@@ -329,11 +330,24 @@ namespace ProjectFirmaModels.Models
         public string ExpectedFundingDiffLog { get; set; }
         public string OrganizationsComment { get; set; }
         public string OrganizationsDiffLog { get; set; }
+        [NotMapped]
+        public HtmlString OrganizationsDiffLogHtmlString
+        { 
+            get { return OrganizationsDiffLog == null ? null : new HtmlString(OrganizationsDiffLog); }
+            set { OrganizationsDiffLog = value?.ToString(); }
+        }
         public string NoExpendituresToReportExplanation { get; set; }
         public string ExpectedPerformanceMeasuresComment { get; set; }
         public string TechnicalAssistanceRequestsComment { get; set; }
         public string ContactsComment { get; set; }
         public string ExpectedFundingUpdateNote { get; set; }
+        public string ContactsDiffLog { get; set; }
+        [NotMapped]
+        public HtmlString ContactsDiffLogHtmlString
+        { 
+            get { return ContactsDiffLog == null ? null : new HtmlString(ContactsDiffLog); }
+            set { ContactsDiffLog = value?.ToString(); }
+        }
         [NotMapped]
         public int PrimaryKey { get { return ProjectUpdateBatchID; } set { ProjectUpdateBatchID = value; } }
 
@@ -377,7 +391,6 @@ namespace ProjectFirmaModels.Models
             public const int GeospatialAreaComment = 1000;
             public const int ExpectedFundingComment = 1000;
             public const int OrganizationsComment = 1000;
-            public const int OrganizationsDiffLog = 1;
             public const int ExpectedPerformanceMeasuresComment = 1000;
             public const int TechnicalAssistanceRequestsComment = 1000;
             public const int ContactsComment = 1000;
