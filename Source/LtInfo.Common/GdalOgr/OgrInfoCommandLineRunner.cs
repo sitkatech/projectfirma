3863e4b453c46d8a8dca4be421d0dc79a52a3338
StewLGSitka <stewart@sitkatech.com>
2019-09-03 10:49:24 -0700

    ProjectFirma: Naming cleanup

--- Source/LtInfo.Common/GdalOgr/OgrInfoCommandLineRunner.cs
+++ Source/LtInfo.Common/GdalOgr/OgrInfoCommandLineRunner.cs
@@ -30,20 +30,20 @@ namespace LtInfo.Common.GdalOgr
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="ogrInfoFileInfo"></param>
+        /// <param name="ogrInfoExecutableFileInfo">Path to the ogrinfo.exe executable</param>
         /// <param name="gdbFileInfo"></param>
         /// <param name="originalFilename">This is the original name of the file as it appeared on the users file system. It is provided just for error messaging purposes.</param>
         /// <param name="totalMilliseconds"></param>
         /// <returns></returns>
-        public static List<string> GetFeatureClassNamesFromFileGdb(FileInfo ogrInfoFileInfo, FileInfo gdbFileInfo, string originalFilename, double totalMilliseconds)
+        public static List<string> GetFeatureClassNamesFromFileGdb(FileInfo ogrInfoExecutableFileInfo, FileInfo gdbFileInfo, string originalFilename, double totalMilliseconds)
         {
             // ReSharper disable once AssignNullToNotNullAttribute
-            var gdalDataDirectory = new DirectoryInfo(Path.Combine(ogrInfoFileInfo.DirectoryName, "gdal-data"));
+            var gdalDataDirectory = new DirectoryInfo(Path.Combine(ogrInfoExecutableFileInfo.DirectoryName, "gdal-data"));
             var commandLineArguments = BuildOgrInfoCommandLineArgumentsToListFeatureClasses(gdbFileInfo, gdalDataDirectory);
-            var processUtilityResult = ProcessUtility.ShellAndWaitImpl(ogrInfoFileInfo.DirectoryName, ogrInfoFileInfo.FullName, commandLineArguments, true, Convert.ToInt32(totalMilliseconds));
+            var processUtilityResult = ProcessUtility.ShellAndWaitImpl(ogrInfoExecutableFileInfo.DirectoryName, ogrInfoExecutableFileInfo.FullName, commandLineArguments, true, Convert.ToInt32(totalMilliseconds));
             if (processUtilityResult.ReturnCode != 0)
             {
-                throw new SitkaDisplayErrorException($"{ogrInfoFileInfo.FullName} unable to open GDB file {gdbFileInfo.FullName} - original filename {originalFilename}.");
+                throw new SitkaDisplayErrorException($"{ogrInfoExecutableFileInfo.FullName} unable to open GDB file {gdbFileInfo.FullName} - original filename {originalFilename}.");
             }
 
             var featureClassesFromFileGdb = processUtilityResult.StdOut.Split(new[] {"\r\n"}, StringSplitOptions.RemoveEmptyEntries).ToList();
