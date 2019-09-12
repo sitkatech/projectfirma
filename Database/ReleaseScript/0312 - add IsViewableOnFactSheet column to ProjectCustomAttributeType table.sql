6364b606e796a3f43a58aca7bfbe05dad8b39883
StewLGSitka <stewart@sitkatech.com>
2019-09-12 14:38:36 -0700

    [projectfirma/#1297] Control visibility of custom project attributes on Fact Sheets Fixing issue with GO's in sql script.

--- Database/ReleaseScript/0312 - add IsViewableOnFactSheet column to ProjectCustomAttributeType table.sql
+++ Database/ReleaseScript/0312 - add IsViewableOnFactSheet column to ProjectCustomAttributeType table.sql
@@ -1,8 +1,10 @@
 ALTER TABLE dbo.ProjectCustomAttributeType
 ADD [IsViewableOnFactSheet] [bit] NULL;
+GO
 
 UPDATE dbo.ProjectCustomAttributeType
 set IsViewableOnFactSheet = 0 where IsViewableOnFactSheet is null;
+GO
 
 ALTER TABLE dbo.ProjectCustomAttributeType
 ALTER COLUMN IsViewableOnFactSheet [bit] NOT NULL;
