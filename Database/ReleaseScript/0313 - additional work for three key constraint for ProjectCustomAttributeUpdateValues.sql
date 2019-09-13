f6ee2fef8b1a0cb1e22bd14ce3e5936da6dc953b
StewLGSitka <stewart@sitkatech.com>
2019-09-13 13:11:17 -0700

    [projectfirma/#1850] Crash when Project Update has duplicate CustomAttribute records Just adding comments - everything works here we believe.

--- Database/ReleaseScript/0313 - additional work for three key constraint for ProjectCustomAttributeUpdateValues.sql
+++ Database/ReleaseScript/0313 - additional work for three key constraint for ProjectCustomAttributeUpdateValues.sql
@@ -1,8 +1,12 @@
+
+-- AK_ProjectCustomAttributeUpdateValue_TenantID_ProjectCustomAttributeUpdateID wasn't quite right; since 
+-- we have multi-selects there can in fact be multiple values selected for a given ProjectCustomAttributeUpdateValueID
 IF OBJECT_ID('dbo.[AK_ProjectCustomAttributeUpdateValue_TenantID_ProjectCustomAttributeUpdateID]', 'UQ') IS NOT NULL
 begin
 
     ALTER TABLE [dbo].ProjectCustomAttributeUpdateValue DROP CONSTRAINT AK_ProjectCustomAttributeUpdateValue_TenantID_ProjectCustomAttributeUpdateID 
 
+    -- But what is enforcable, and bad, is to have the exact SAME value. This IS a problem, and we'll trap for that.
     ALTER TABLE [dbo].ProjectCustomAttributeUpdateValue ADD CONSTRAINT AK_ProjectCustomAttributeUpdateValue_TenantID_ProjectCustomAttributeUpdateID_AttributeValue UNIQUE NONCLUSTERED
     (
         TenantID ASC,
