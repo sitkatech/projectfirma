d0f72f782ff6b6e7314ee0716753ab5cd8c7e65b
bGrass <brazen8s@gmail.com>
2019-09-19 10:26:09 -0700

    Update Fact Sheet footer for Peaks to People - run as hotfix

---
+++ Database/Hotfixes/20190919 - Update Fact Sheet footer for Peaks to People.sql
@@ -0,0 +1,2 @@
+Update dbo.FirmaPage set FirmaPageContent = '<p><em>Accomplishments are modeled through Peaks to People’s Watershed Investment Tool and Co-benefits Module. Conditional metrics do not account for the probability of fire. Expected metrics account for the probability the treated area is burned by wildfire. For more information, please visit <a href="https://peakstopeople.org/our-solutions/" target="_blank">https://peakstopeople.org/our-solutions/</a></em></p>'
+where TenantID = 6 and FirmaPageTypeID = 54
