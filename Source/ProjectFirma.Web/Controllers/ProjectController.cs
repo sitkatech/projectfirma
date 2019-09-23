5d88f6058d127ce566215982e914f4d8226bf5aa
stewart-gordon-sitka <stewart.gordon@sitkatech.com>
2019-09-23 09:12:43 -0700

    [projectfirma/#1297] Control visibility of custom project attributes on Fact Sheets Fix for failing test related to this new feature

--- Source/ProjectFirma.Web/Controllers/ProjectController.cs
+++ Source/ProjectFirma.Web/Controllers/ProjectController.cs
@@ -912,6 +912,7 @@ Continue with a new {FieldDefinitionEnum.Project.ToType().GetFieldDefinitionLabe
             }
         }
 
+        [ProjectsViewFullListFeature]
         public FileContentResult FactSheetWithCustomAttributesPdf(ProjectPrimaryKey projectPrimaryKey)
         {
             var project = projectPrimaryKey.EntityObject;
