59ad2ed97a9710bfcaa05aacf30a3fa5f6c2b5f3
stewart-gordon-sitka <stewart.gordon@sitkatech.com>
2019-09-26 14:05:26 -0700

    [projectfirma/#1816] Enhance Custom Attributes to Capture Administrative Approval and Authorization #s Update release script to backfill the custom attribute type orders to the order that they were created (preserving the current state that tenants are used to in Project Firma)

--- Database/ReleaseScript/0318 - adding sortOrder to ProjectCustomAttributeType.sql
+++ Database/ReleaseScript/0318 - adding sortOrder to ProjectCustomAttributeType.sql
@@ -1,2 +1,12 @@
 alter table dbo.ProjectCustomAttributeType
-add SortOrder int null
+add SortOrder int null;
+
+GO
+
+-- even though the application uses a different magic number to sort, 
+-- creating an initial sort order that is based off of their incrementing ID will
+-- preserve the order that the Tenants currently have in Project Firma
+update dbo.ProjectCustomAttributeType
+set SortOrder = ProjectCustomAttributeTypeID;
+
+GO
