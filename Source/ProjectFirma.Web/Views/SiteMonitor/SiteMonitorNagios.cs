d0b66a1444ec5632062d0749b77aa3b562bee288
StewLGSitka <stewart@sitkatech.com>
2019-09-27 14:47:17 -0700

    ProjectFirma: Adding health checks.
    This adds the framework for a working page (https://actionagendatracker.localhost.projectfirma.com/SiteMonitor/SiteMonitor for example), but does not actually have any health checks yet.

---
+++ Source/ProjectFirma.Web/Views/SiteMonitor/SiteMonitorNagios.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Web.Mvc;
+using LtInfo.Common.HealthMonitor;
+using LtInfo.Common.Mvc;
+using ProjectFirma.Web.Models;
+using ProjectFirma.Web.Views.ProjectFundingSourceBudget;
+using ProjectFirmaModels.Models;
+
+namespace ProjectFirma.Web.Views.SiteMonitor
+{
+    /*
+     *
+     * namespace ProjectFirma.Web.Views.Shared
+{
+    public abstract class DisplayPageContent : LtInfo.Common.Mvc.TypedWebViewPage<DisplayPageContentViewData>
+    {
+    }
+}
+     */
+
+
+    public abstract class SiteMonitorNagios : LtInfo.Common.Mvc.TypedWebViewPage<SiteMonitorNagiosViewData>
+    {
+        protected string GetStatusAsColorStyle(HealthCheckStatus status)
+        {
+            string color;
+            switch (status)
+            {
+                case HealthCheckStatus.OK:
+                    color = "green";
+                    break;
+                case HealthCheckStatus.Warning:
+                    color = "orange";
+                    break;
+                case HealthCheckStatus.Critical:
+                    color = "red";
+                    break;
+                case HealthCheckStatus.Unknown:
+                    color = "orange";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(status), status, null);
+            }
+            return $"color:{color}";
+        }
+    }
+}
+
+namespace ProjectFirma.Web.Views.SiteMonitor
+{
+    public class SiteMonitorNagiosViewData : FirmaViewData
+    {
+        public readonly HealthCheckResults HealthCheckResults;
+        public readonly string CompleteNagiosOutput;
+        public readonly int NagiosReturnCode;
+
+        public SiteMonitorNagiosViewData(Person currentPerson, HealthCheckResults healthCheckResults) : base(currentPerson)
+        {
+            HealthCheckResults = healthCheckResults;
+            CompleteNagiosOutput = HealthCheckResults.GetHealthCheckResultsAsCompleteNagiosOutputText();
+            NagiosReturnCode = HealthCheckResults.GetNagiosReturnCode();
+        }
+    }
+}
+
+
+
