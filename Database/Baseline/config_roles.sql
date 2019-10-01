7bc0e6945f157569818b069bf3fa914801c54c6b
raynardlee <raynardlee@yahoo.com>
2019-10-01 15:12:34 -0700

    needed to add accounttype of G to differentiate between users and groups

--- Database/Baseline/config_roles.sql
+++ Database/Baseline/config_roles.sql
@@ -2,46 +2,47 @@ use ${db-name}
 
 print 'Getting logins to create'
 if object_id('tempdb.dbo.#accountsToCreate') is not null drop table #accountsToCreate
-select '${db-user}' as AccountName
+select '${db-user}' as AccountName, 'U' as AccountType
 into #accountsToCreate
-union select '${db-batch-user}'
-union select '${db-geoserver-user}'
-union select 'Sitka\Rocket QA Support'
-union select 'Sitka\Rocket QA Tester'
+union select '${db-batch-user}', 'U'
+union select '${db-geoserver-user}', 'U'
+union select 'Sitka\Rocket QA Support', 'G'
+union select 'Sitka\Rocket QA Tester', 'G'
 
 -- Clear out any existing logins
 print 'Clearing out any existing logins'
 
 if object_id('tempdb.dbo.#allAccounts') is not null drop table #allAccounts
-select a.AccountName
+select a.AccountName, a.AccountType
 into #allAccounts
 from
 (
-    select '${local-db-user}' as AccountName
-    union select '${qa-db-user}'
-    union select '${prod-db-user}'
-    union select '${local-db-batch-user}'
-    union select '${qa-db-batch-user}'
-    union select '${prod-db-batch-user}'
-	union select '${local-db-geoserver-user}'
-	union select '${qa-db-geoserver-user}'
-	union select '${prod-db-geoserver-user}'
-    union select 'Sitka\Rocket QA Support'
-    union select 'Sitka\Rocket QA Tester'
+    select '${local-db-user}' as AccountName, 'U' as AccountType
+    union select '${qa-db-user}', 'U'
+    union select '${prod-db-user}', 'U'
+    union select '${local-db-batch-user}', 'U'
+    union select '${qa-db-batch-user}', 'U'
+    union select '${prod-db-batch-user}', 'U'
+	union select '${local-db-geoserver-user}', 'U'
+	union select '${qa-db-geoserver-user}', 'U'
+	union select '${prod-db-geoserver-user}', 'U'
+    union select 'Sitka\Rocket QA Support', 'G'
+    union select 'Sitka\Rocket QA Tester', 'G'
 ) a left join #accountsToCreate ac on a.AccountName = ac.AccountName
 where ac.AccountName is null
 
 
 declare @accountName varchar(200)
+declare @accountType varchar(2)
 declare @sql nvarchar(1000)
 
 while exists(select 1 from #allAccounts)
 begin
-    select top 1 @accountName = AccountName from #allAccounts
+    select top 1 @accountName = AccountName, @accountType = AccountType from #allAccounts
 
     print 'Login: ' + @accountName
     
-    if exists (select 1 from sys.server_principals where name = @accountName and type = 'U')
+    if exists (select 1 from sys.server_principals where name = @accountName and type = @accountType)
     begin
         set @sql = replace('drop login [ACCOUNT]', 'ACCOUNT',  @accountName)
         print @sql
@@ -64,11 +65,11 @@ print 'Creating logins for this environment'
 
 while exists(select 1 from #accountsToCreate)
 begin
-    select top 1 @accountName = AccountName from #accountsToCreate
+    select top 1 @accountName = AccountName, @accountType = AccountType from #accountsToCreate
 
     print 'Login: ' + @accountName
 
-    if not exists (select 1 from sys.server_principals where name = @accountName and type = 'U')
+    if not exists (select 1 from sys.server_principals where name = @accountName and type = @accountType)
     begin
         set @sql = replace('create login [ACCOUNT] from windows', 'ACCOUNT',  @accountName)
         print @sql
