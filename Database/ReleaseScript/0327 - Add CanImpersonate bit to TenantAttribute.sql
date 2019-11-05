41c3a1eabb5e626ff774907daf3534003ec02024
StewLGSitka <stewart@sitkatech.com>
2019-11-04 17:44:44 -0800

    [projectfirma/#1827] Implement Impersonation in PF - Button now appears, and we get a crash when we try to use it. Progress!

--- Database/ReleaseScript/0327 - Add CanImpersonate bit to TenantAttribute.sql
+++ Database/ReleaseScript/0327 - Add CanImpersonate bit to TenantAttribute.sql
@@ -12,11 +12,11 @@ alter table dbo.TenantAttribute
 alter column ImpersonationEnabled bit not null
 GO
 
--- Enabled only for BOR to start.
+-- Enabled only for BOR and PSP for now.
 -- If you want to play too, add your tenant in.
 update dbo.TenantAttribute
 set ImpersonationEnabled = 1
-where TenantID = 12
+where TenantID = 12 or TenantID = 11
 
 --rollback tran
 
