9ff94e952471c89fbef533b997f8a47ca04c6aa8
StewLGSitka <stewart@sitkatech.com>
2019-11-11 10:49:59 -0800

    [projectfirma/#1952] Deal with SQL type issues on Reclamation/PF - Test up and doing something.

--- Source/ProjectFirma.Web/HealthMonitor/SqlServerSpatialDllSeemsToWork.cs
+++ Source/ProjectFirma.Web/HealthMonitor/SqlServerSpatialDllSeemsToWork.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Web;
+using LtInfo.Common.DesignByContract;
 using LtInfo.Common.HealthMonitor;
 using Microsoft.SqlServer.Types;
+using ProjectFirma.Web.Common;
 
 namespace ProjectFirma.Web.HealthMonitor
 {
@@ -33,7 +36,18 @@ namespace ProjectFirma.Web.HealthMonitor
 
             try
             {
-                //DatabaseModels.Geometry.SqlGeometryUtilities.AreSqlGeometriesEqual(geom1, geom2);
+                // Pull some areas from DB...
+                // --------------------------
+                Check.Ensure(HttpRequestStorage.DatabaseEntities.GeospatialAreas.Any(),
+                    "No Geospatial areas found in HttpRequestStorage.DatabaseEntities; expecting at least Watersheds for example. If this not true for your tenant, modify this health check.");
+
+                var aGeoSpatialArea = HttpRequestStorage.DatabaseEntities.GeospatialAreas.First();
+                Check.EnsureNotNull(aGeoSpatialArea);
+                Check.EnsureNotNull(aGeoSpatialArea.GeospatialAreaFeature);
+
+                // Make sure Sql Server Spatial DLL functions properly
+                // These should not throw
+                SqlGeometryUtilities.AreSqlGeometriesEqual(geom1, geom2);
 
             }
             catch (Exception e)
@@ -42,6 +56,34 @@ namespace ProjectFirma.Web.HealthMonitor
                 return false;
             }
             return true;
+        }
+
+
+        public static class SqlGeometryUtilities
+        {
+            /// <summary>
+            /// Checks if two <see cref="SqlGeometry"/> instances are equal, works on null also. Null equals Null in this case. Also handles <see cref="INullable.IsNull"/> so <see cref="SqlGeometry.Null"/> equals <see cref="SqlGeometry.Null"/> 
+            /// </summary>
+            public static bool AreSqlGeometriesEqual(SqlGeometry lhs, SqlGeometry rhs)
+            {
+                if (lhs == null && rhs == null)
+                {
+                    return true;
+                }
+                if (lhs == null ^ rhs == null)
+                {
+                    return false;
+                }
+                if (lhs.IsNull && rhs.IsNull)
+                {
+                    return true;
+                }
+                if (lhs.IsNull ^ rhs.IsNull)
+                {
+                    return false;
+                }
+                return lhs.STEquals(rhs).IsTrue;
+            }
         }
 
     }
