364508a128b9057daa8885a5bf55f050b93d595f
stewart-gordon-sitka <stewart.gordon@sitkatech.com>
2019-11-21 14:39:22 -0800

    [projectfirma/#1900] * adding IAuditableEntity to ProjectStatus

---
+++ Source/ProjectFirmaModels/Models/ProjectStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectFirmaModels.Models
+{
+    public partial class ProjectStatus : IAuditableEntity
+    {
+        public string GetAuditDescriptionString()
+        {
+            var projectStatusProjectStatusDisplayName = ProjectStatusDisplayName;
+            var tenantID = TenantID;
+            return $"Project Status: Project Status - {projectStatusProjectStatusDisplayName}, Tenant - {tenantID}";
+        }
+    }
+}
