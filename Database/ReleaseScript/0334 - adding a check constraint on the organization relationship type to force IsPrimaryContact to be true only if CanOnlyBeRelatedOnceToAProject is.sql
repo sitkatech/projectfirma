15cade9c003307c5b334b376774d639268c03cbe
bGrass <brazen8s@gmail.com>
2019-11-27 12:39:20 -0800

    Delete release scripts that were released to Prod last night

--- Database/ReleaseScript/0334 - adding a check constraint on the organization relationship type to force IsPrimaryContact to be true only if CanOnlyBeRelatedOnceToAProject is.sql
+++
@@ -1,9 +0,0 @@
-
-ALTER TABLE [dbo].[OrganizationRelationshipType]  WITH CHECK ADD  CONSTRAINT [CK_OrganizationRelationshipType_CanOnlyBeRelatedOnceToAProjectMustBeTrueIfIsPrimaryContact] 
-
-CHECK  ((IsPrimaryContact = 1 and CanOnlyBeRelatedOnceToAProject = 1) or (  IsPrimaryContact = 0 ))
-GO
-
-ALTER TABLE [dbo].[OrganizationRelationshipType] CHECK CONSTRAINT [CK_OrganizationRelationshipType_CanOnlyBeRelatedOnceToAProjectMustBeTrueIfIsPrimaryContact]
-GO
-
