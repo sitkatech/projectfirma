17245b7b6ebe07857aa48819e729227d31d72570
stewartgordonsitka <stewart.gordon@sitkatech.com>
2019-12-03 16:56:51 -0800

    [projectfirma/#1901] Set performance measure targets for a specific geospatial area * Add AK for year and tenantID

--- Database/ReleaseScript/0342 - consolidate PerformanceMeasureReportingPeriod.sql
+++ Database/ReleaseScript/0342 - consolidate PerformanceMeasureReportingPeriod.sql
@@ -146,3 +146,10 @@
 
 
   */
+
+ALTER TABLE [dbo].[PerformanceMeasureReportingPeriod] ADD  CONSTRAINT [AK_PerformanceMeasureReportingPeriod_TenantID_PerformanceMeasureReportingPeriodCalendarYear] UNIQUE NONCLUSTERED 
+(
+	[TenantID] ASC,
+	[PerformanceMeasureReportingPeriodCalendarYear] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
