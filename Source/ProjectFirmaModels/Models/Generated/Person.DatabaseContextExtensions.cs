3ac95ef0c818a2318df2bbaaa2f739e909f94819
stewartgordonsitka <stewart.gordon@sitkatech.com>
2019-12-10 17:22:06 -0800

    [projectfirma/#1963] Expose project web feature service * Prevent crashes primarily due to the static constructor for the WebServiceToken

--- Source/ProjectFirmaModels/Models/Generated/Person.DatabaseContextExtensions.cs
+++ Source/ProjectFirmaModels/Models/Generated/Person.DatabaseContextExtensions.cs
@@ -12,11 +12,19 @@ namespace ProjectFirmaModels.Models
 {
     public static partial class DatabaseContextExtensions
     {
-        public static Person GetPerson(this IQueryable<Person> people, int personID)
+        public static Person GetPerson(this IQueryable<Person> people, int personID, bool throwIfNotFound)
         {
             var person = people.SingleOrDefault(x => x.PersonID == personID);
-            Check.RequireNotNullThrowNotFound(person, "Person", personID);
+            if (throwIfNotFound)
+            {
+                Check.RequireNotNullThrowNotFound(person, "Person", personID);
+            }
             return person;
+        }
+
+        public static Person GetPerson(this IQueryable<Person> people, int personID)
+        {
+            return GetPerson(people, personID, true);
         }
 
     }
