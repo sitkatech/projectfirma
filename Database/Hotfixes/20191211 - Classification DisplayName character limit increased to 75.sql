35d4920d9bfc0f9c1d914908f66add7a22c33ab6
bGrass <brazen8s@gmail.com>
2019-12-11 14:56:43 -0800

    Revert "Classification DisplayName character limit increased to 75 - run as hotfix on Prod"
    This reverts commit bbaef993cea55e5a46174aa8e5040db3c944ad9e.

--- Database/Hotfixes/20191211 - Classification DisplayName character limit increased to 75.sql
+++
@@ -1,6 +0,0 @@
-
-Alter table dbo.Classification alter column DisplayName varchar(75) not null
-
-Update dbo.Organization set OrganizationName = 'Gilliam County Soil and Water Conservation District' where OrganizationID = 6340
-
-
