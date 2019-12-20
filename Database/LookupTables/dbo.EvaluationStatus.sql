0fcfbdabd36d95718ce541428602acf31ed6b1d0
tomkamin <tom.kamin@sitkatech.com>
2019-12-20 14:41:45 -0800

    [projectfirma/#1182] Define project evaluation criteria (as an admin) fixing spaces in lookup tables

--- Database/LookupTables/dbo.EvaluationStatus.sql
+++ Database/LookupTables/dbo.EvaluationStatus.sql
@@ -2,5 +2,5 @@ delete from dbo.EvaluationStatus
 
 insert into dbo.EvaluationStatus(EvaluationStatusName, EvaluationStatusDisplayName) values ('Draft','Draft')
 insert into dbo.EvaluationStatus(EvaluationStatusName, EvaluationStatusDisplayName) values ('Planned','Planned')
-insert into dbo.EvaluationStatus(EvaluationStatusName, EvaluationStatusDisplayName) values ('InProgress','InProgress')
+insert into dbo.EvaluationStatus(EvaluationStatusName, EvaluationStatusDisplayName) values ('InProgress','In Progress')
 insert into dbo.EvaluationStatus(EvaluationStatusName, EvaluationStatusDisplayName) values ('Completed','Completed')
