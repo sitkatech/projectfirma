873b5970d4a6af8fd7c48b2e8128dac07ebbee1d
john.vivio@sitkatech.com <ILoveCoho123!>
2019-12-30 12:30:27 -0800

    [projectfirma/#1962] Capture 'lessons learned' for a completed project - adding tenant configuration to control turning this feature on and off

--- Database/ReleaseScript/0352 - changing Reporting Year to Year in existing chart configurations for performance measures.sql
+++
@@ -1,5 +0,0 @@
-
-
-update dbo.PerformanceMeasureSubcategory set ChartConfigurationJson = REPLACE(ChartConfigurationJson, '"Reporting Year"', '"Year"');
-
-update dbo.PerformanceMeasureSubcategory set CumulativeChartConfigurationJson = REPLACE(CumulativeChartConfigurationJson, '"Reporting Year"', '"Year"');
