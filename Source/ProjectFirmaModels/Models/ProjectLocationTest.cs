67dd07c115355c7e412f09e2dcdece3336c12981
stewartgordonsitka <stewart.gordon@sitkatech.com>
2019-12-30 13:27:34 -0800

    [projectfirma/#1963] Expose project web feature service * Add test to ensure that ProjectLocation geometries are all valid

---
+++ Source/ProjectFirmaModels/Models/ProjectLocationTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LtInfo.Common.DesignByContract;
+using NUnit.Framework;
+using ProjectFirmaModels.UnitTestCommon;
+
+namespace ProjectFirmaModels.Models
+
+{
+    [TestFixture]
+    public class ProjectLocationTest
+    {
+        [Test]
+        public void TestProjectLocationGeometriesAreAllValid()
+        {
+            var invalidProjectLocationGeometriesList = new List<ProjectLocation>();
+
+            var projectLocations = HttpRequestStorageForTest.DatabaseEntities.AllProjectLocations.ToList();
+            foreach (var projectLocation in projectLocations)
+            {
+                if (!projectLocation.ProjectLocationGeometry.IsValid)
+                {
+                    invalidProjectLocationGeometriesList.Add(projectLocation);
+                }
+            }
+
+            Check.Ensure(!invalidProjectLocationGeometriesList.Any(), $"Found invalid geometries for ProjectLocationIDs: {String.Join(",", invalidProjectLocationGeometriesList.Select(x => x.ProjectLocationID).ToList())}");
+        }
+    }
+}
