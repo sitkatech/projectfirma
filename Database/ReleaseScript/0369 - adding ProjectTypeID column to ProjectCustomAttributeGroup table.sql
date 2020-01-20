6569fec142157d3d9f34aa6e74f43e6a7f9ca6ac
stewartgordonsitka <stewart.gordon@sitkatech.com>
2020-01-20 13:54:52 -0800

    [projectfirma/#1869] Control Visibility of Custom Attributes by Project Type * Add ProjectTypeID to ProjectCustomAttributeGroup table

---
+++ Database/ReleaseScript/0369 - adding ProjectTypeID column to ProjectCustomAttributeGroup table.sql
@@ -0,0 +1,14 @@
+﻿--begin tran
+
+ALTER TABLE dbo.[ProjectCustomAttributeGroup] 
+ADD ProjectTypeID int not null DEFAULT 1
+GO
+
+ALTER TABLE [dbo].[ProjectCustomAttributeGroup]  WITH CHECK ADD  CONSTRAINT [FK_ProjectCustomAttributeGroup_ProjectType_ProjectTypeID] FOREIGN KEY([ProjectTypeID])
+REFERENCES [dbo].[ProjectType] ([ProjectTypeID])
+GO
+
+ALTER TABLE [dbo].[ProjectCustomAttributeGroup] CHECK CONSTRAINT [FK_ProjectCustomAttributeGroup_ProjectType_ProjectTypeID]
+GO
+
+--rollback tran
