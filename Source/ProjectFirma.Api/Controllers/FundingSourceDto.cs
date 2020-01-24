1a6cc5e6d6c49be2481eb5856aa63e3893bfb2ce
Shannon Bulloch <shannon.bulloch@sitkatech.com>
2020-01-24 10:27:31 -0800

    [delta/#398] [delta/#399] [delta/#401] added APIs to add, update and delete funding sources and organizations in PF from PS Info

---
+++ Source/ProjectFirma.Api/Controllers/FundingSourceDto.cs
@@ -0,0 +1,29 @@
+﻿using ProjectFirmaModels.Models;
+
+namespace ProjectFirma.Api.Controllers
+{
+    public class FundingSourceDto
+    {
+        public FundingSourceDto(FundingSource fundingSource)
+        {
+            FundingSourceID = fundingSource.FundingSourceID;
+            OrganizationID = fundingSource.OrganizationID;
+            FundingSourceName = fundingSource.FundingSourceName;
+            IsActive = fundingSource.IsActive;
+            FundingSourceDescription = fundingSource.FundingSourceDescription;
+            FundingSourceAmount = fundingSource.FundingSourceAmount;
+        }
+
+        public FundingSourceDto()
+        {
+        }
+
+        public int FundingSourceID { get; set; }
+        public int OrganizationID { get; set; }
+        public string FundingSourceName { get; set; }
+        public bool IsActive { get; set; }
+        public string FundingSourceDescription { get; set; }
+        public decimal? FundingSourceAmount { get; set; }
+    }
+
+}
