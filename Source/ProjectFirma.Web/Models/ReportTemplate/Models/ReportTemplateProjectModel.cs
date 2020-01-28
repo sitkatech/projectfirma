940c7cb33fc68445aa7e5fd868d3f322b957a715
stewartgordonsitka <stewart.gordon@sitkatech.com>
2020-01-27 17:20:41 -0800

    [projectfirma/#1903] Implement word templating library for Annual Reports * Add base models for the Report Templates

--- Source/ProjectFirma.Web/Models/ReportTemplate/Models/ReportTemplateProjectModel.cs
+++ Source/ProjectFirma.Web/Models/ReportTemplate/Models/ReportTemplateProjectModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using ProjectFirmaModels.Models;
 using LtInfo.Common;
@@ -7,43 +8,99 @@ namespace ProjectFirma.Web.Models.ReportTemplate.Models
 {
     public class ReportTemplateProjectModel : ReportTemplateBaseModel
     {
-        public string ProjectName { get; set; }
-        public string ProjectDescription { get; set; }
-        public string ApprovalDate { get; set; }
-        public string SubmissionDate { get; set; }
-        public string CompletionYear { get; set; }
-        public string ImplementationStartYear { get; set; }
-        public string ProjectStatusColor { get; set; }
-        public string ProjectStatusName { get; set; }
-        public string ProjectStage { get; set; }
-        public string EstimatedTotalCost { get; set; }
 
+        private Project Project { get; set; }
         private List<ProjectContact> ProjectContacts { get; set; }
         private List<ProjectOrganization> ProjectOrganizations { get; set; }
 
+
+        public string ProjectName { get; set; }
+        public string ProjectUrl { get; set; }
+        public ReportTemplateOrganizationModel PrimaryContactOrganization { get; set; }
+        public string ProjectStage { get; set; }
+        public int NumberOfReportedPerformanceMeasures { get; set; }
+        public ReportTemplatePersonModel ProjectPrimaryContact { get; set; }
+        public string PlanningDesignStartYear { get; set; }
+        public string ImplementationStartYear { get; set; }
+        public string CompletionYear { get; set; }
+        public string PrimaryTaxonomyLeaf { get; set; }
+        public int NumberOfReportedExpenditures { get; set; }
+        public string FundingType { get; set; }
+        public string EstimatedTotalCost { get; set; }
+        public string SecuredFunding { get; set; }
+        public string TargetedFunding { get; set; }
+        public string NoFundingSourceIdentified { get; set; }
+        public string ProjectDescription { get; set; }
+        public int ProjectID { get; set; }
+        public DateTime ProjectLastUpdated { get; set; }
+        public string ProjectStatus { get; set; }
+        public string ProjectStatusColor { get; set; }
+        public string FinalStatusUpdateStatus { get; set; }
+
+
         public ReportTemplateProjectModel(Project project)
         {
-            ProjectName = project.ProjectName;
-            ProjectDescription = project.ProjectDescription;
-            ApprovalDate = project.ApprovalDate.ToString();
-            SubmissionDate = project.SubmissionDate.ToString();
-            CompletionYear = project.CompletionYear.ToString();
-            ImplementationStartYear = project.ImplementationStartYear.ToString();
+            // Private properties
+            Project = project;
+            ProjectContacts = project.ProjectContacts.ToList();
+            ProjectOrganizations = project.ProjectOrganizations.ToList();
+
+            // Public properties
+            ProjectName = Project.ProjectName;
+            ProjectUrl = Project.GetDetailUrl();
+            PrimaryContactOrganization = new ReportTemplateOrganizationModel(Project.GetPrimaryContactOrganization());
+            
+            ProjectStage = Project.ProjectStage.ProjectStageDisplayName;
+            NumberOfReportedPerformanceMeasures = Project.PerformanceMeasureActuals.Count;
+            ProjectPrimaryContact = new ReportTemplatePersonModel(Project.GetPrimaryContact());
+            PlanningDesignStartYear = ProjectModelExtensions.GetPlanningDesignStartYear(Project);
+            ImplementationStartYear = ProjectModelExtensions.GetImplementationStartYear(Project);
+            CompletionYear = ProjectModelExtensions.GetCompletionYear(Project);
+            PrimaryTaxonomyLeaf = Project.TaxonomyLeaf.GetDisplayName();
+            NumberOfReportedExpenditures = Project.ProjectFundingSourceExpenditures.Count;
+            FundingType = Project.FundingType.FundingTypeDisplayName;
+            EstimatedTotalCost = Project.GetEstimatedTotalRegardlessOfFundingType().ToStringCurrency();
+            SecuredFunding = Project.GetSecuredFunding().ToStringCurrency();
+            TargetedFunding = Project.GetTargetedFunding().ToStringCurrency();
+            NoFundingSourceIdentified = Project.GetNoFundingSourceIdentifiedAmount().ToStringCurrency();
+            ProjectDescription = Project.ProjectDescription;
+            ProjectID = Project.ProjectID;
+            ProjectLastUpdated = Project.LastUpdatedDate;
 
             var projectStatus = project.GetCurrentProjectStatus();
             if (projectStatus != null)
             {
                 ProjectStatusColor = projectStatus.ProjectStatusColor;
-                ProjectStatusName = projectStatus.ProjectStatusDisplayName;
+                ProjectStatus = projectStatus.ProjectStatusDisplayName;
             }
 
-            ProjectContacts = project.ProjectContacts.ToList();
-            ProjectOrganizations = project.ProjectOrganizations.ToList();
+            var finalProjectStatus = Project.FinalStatusReportStatusDescription;
+            if (finalProjectStatus != null)
+            {
+                FinalStatusUpdateStatus = finalProjectStatus;
+            }
+        }
 
-            ProjectStage = project.ProjectStage.ProjectStageDisplayName;
+        public List<ReportTemplatePersonModel> GetContacts()
+        {
+            return ProjectContacts.Select(x => new ReportTemplatePersonModel(x.Contact)).ToList();
+        }
 
-            EstimatedTotalCost = project.GetEstimatedTotalRegardlessOfFundingType().ToStringCurrency();
+        public List<ReportTemplatePersonModel> GetContactsByType(string contactTypeName)
+        {
+            var projectContactsInType = ProjectContacts.Where(x => x.ContactRelationshipType.ContactRelationshipTypeName == contactTypeName).ToList();
+            return projectContactsInType.Select(x => new ReportTemplatePersonModel(x.Contact)).ToList();
+        }
 
+        public List<ReportTemplateOrganizationModel> GetOrganizations()
+        {
+            return ProjectOrganizations.Select(x => new ReportTemplateOrganizationModel(x.Organization)).ToList();
+        }
+
+        public List<ReportTemplateOrganizationModel> GetOrganizationsByType(string organizationTypeName)
+        {
+            var organizationsInType = ProjectOrganizations.Where(x => x.OrganizationRelationshipType.OrganizationRelationshipTypeName == organizationTypeName).ToList();
+            return organizationsInType.Select(x => new ReportTemplateOrganizationModel(x.Organization)).ToList();
         }
 
         public string GetProjectContactNamesByType(string contactTypeName)
