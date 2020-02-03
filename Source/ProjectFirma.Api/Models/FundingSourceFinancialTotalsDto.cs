5bdfe0cddcbf80f3911643339ed3ec862c5ee8d7
bGrass <brazen8s@gmail.com>
2020-02-03 09:21:15 -0800

    Add enpoint for financial summary totals for Funding Sources

---
+++ Source/ProjectFirma.Api/Models/FundingSourceFinancialTotalsDto.cs
@@ -0,0 +1,25 @@
+﻿using ProjectFirmaModels.Models;
+using System.Linq;
+
+namespace ProjectFirma.Api.Models
+{
+    public class FundingSourceFinancialTotalsDto
+    {
+        public FundingSourceFinancialTotalsDto(FundingSource fundingSource, IQueryable<ProjectFundingSourceBudget> projectFundingSourceBudgets, IQueryable<ProjectFundingSourceExpenditure> projectFundingSourceExpenditures)
+        {
+            ProjectFirmaFundingSourceID = fundingSource.FundingSourceID;
+            BudgetSecuredTotal = projectFundingSourceBudgets.Any(x => x.SecuredAmount != null) ? projectFundingSourceBudgets.Sum(x => x.SecuredAmount) : null;
+            ExpendituresTotal = projectFundingSourceExpenditures.Any() ? projectFundingSourceExpenditures.Sum(x => x.ExpenditureAmount) : (decimal?) null;
+        }
+
+        public FundingSourceFinancialTotalsDto()
+        {
+        }
+
+        public int ProjectFirmaFundingSourceID { get; set; }
+        public decimal? BudgetSecuredTotal { get; set; }
+        public decimal? ExpendituresTotal { get; set; }
+    }
+
+
+}
