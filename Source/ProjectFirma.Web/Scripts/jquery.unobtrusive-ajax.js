c46069cf420809bb0ea15389365052a73ca9b81b
bGrass <brazen8s@gmail.com>
2020-04-28 13:41:41 -0700

    Revert "ProjectFirma: Library upgrades. I'm squashing this in case something goes awry for easy reversion. There's a lot more to upgrade, but this is a start."
    This reverts commit aea5ecac3a93b0c08db96bd6850ba556fa3bbf9c.

--- Source/ProjectFirma.Web/Scripts/jquery.unobtrusive-ajax.js
+++ Source/ProjectFirma.Web/Scripts/jquery.unobtrusive-ajax.js
@@ -1,18 +1,21 @@
-// Unobtrusive Ajax support library for jQuery
-// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-// @version v3.2.6
-// 
-// Microsoft grants you the right to use these script files for the sole
-// purpose of either: (i) interacting through your browser with the Microsoft
-// website or online service, subject to the applicable licensing or use
-// terms; or (ii) using the files as included with a Microsoft product subject
-// to that product's license terms. Microsoft reserves all other rights to the
-// files not expressly granted by Microsoft, whether by implication, estoppel
-// or otherwise. Insofar as a script file is dual licensed under GPL,
-// Microsoft neither took the code under GPL nor distributes it thereunder but
-// under the terms set out in this paragraph. All notices and licenses
-// below are for informational purposes only.
+/* NUGET: BEGIN LICENSE TEXT
+ *
+ * Microsoft grants you the right to use these script files for the sole
+ * purpose of either: (i) interacting through your browser with the Microsoft
+ * website or online service, subject to the applicable licensing or use
+ * terms; or (ii) using the files as included with a Microsoft product subject
+ * to that product's license terms. Microsoft reserves all other rights to the
+ * files not expressly granted by Microsoft, whether by implication, estoppel
+ * or otherwise. Insofar as a script file is dual licensed under GPL,
+ * Microsoft neither took the code under GPL nor distributes it thereunder but
+ * under the terms set out in this paragraph. All notices and licenses
+ * below are for informational purposes only.
+ *
+ * NUGET: END LICENSE TEXT */
+/*!
+** Unobtrusive Ajax support library for jQuery
+** Copyright (C) Microsoft Corporation. All rights reserved.
+*/
 
 /*jslint white: true, browser: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: false */
 /*global window: false, jQuery: false */
@@ -56,18 +59,23 @@
             var top;
 
             switch (mode) {
-                case "BEFORE":
-                    $(update).prepend(data);
-                    break;
-                case "AFTER":
-                    $(update).append(data);
-                    break;
-                case "REPLACE-WITH":
-                    $(update).replaceWith(data);
-                    break;
-                default:
-                    $(update).html(data);
-                    break;
+            case "BEFORE":
+                top = update.firstChild;
+                $("<div />").html(data).contents().each(function () {
+                    update.insertBefore(this, top);
+                });
+                break;
+            case "AFTER":
+                $("<div />").html(data).contents().each(function () {
+                    update.appendChild(this);
+                });
+                break;
+            case "REPLACE-WITH":
+                $(update).replaceWith(data);
+                break;
+            default:
+                $(update).html(data);
+                break;
             }
         });
     }
@@ -86,7 +94,7 @@
         $.extend(options, {
             type: element.getAttribute("data-ajax-method") || undefined,
             url: element.getAttribute("data-ajax-url") || undefined,
-            cache: (element.getAttribute("data-ajax-cache") || "").toLowerCase() === "true",
+            cache: !!element.getAttribute("data-ajax-cache"),
             beforeSend: function (xhr) {
                 var result;
                 asyncOnBeforeSend(xhr, method);
@@ -116,30 +124,6 @@
             options.type = "POST";
             options.data.push({ name: "X-HTTP-Method-Override", value: method });
         }
-
-        // change here:
-        // Check for a Form POST with enctype=multipart/form-data
-        // add the input file that were not previously included in the serializeArray()
-        // set processData and contentType to false
-        var $element = $(element);
-        if ($element.is("form") && $element.attr("enctype") == "multipart/form-data") {
-            var formdata = new FormData();
-            $.each(options.data, function (i, v) {
-                formdata.append(v.name, v.value);
-            });
-            $("input[type=file]", $element).each(function () {
-                var file = this;
-                $.each(file.files, function (n, v) {
-                    formdata.append(file.name, v);
-                });
-            });
-            $.extend(options, {
-                processData: false,
-                contentType: false,
-                data: formdata
-            });
-        }
-        // end change
 
         $.ajax(options);
     }
@@ -191,7 +175,7 @@
     $(document).on("submit", "form[data-ajax=true]", function (evt) {
         var clickInfo = $(this).data(data_click) || [],
             clickTarget = $(this).data(data_target),
-            isCancel = clickTarget && (clickTarget.hasClass("cancel") || clickTarget.attr('formnovalidate') !== undefined);
+            isCancel = clickTarget && clickTarget.hasClass("cancel");
         evt.preventDefault();
         if (!isCancel && !validate(this)) {
             return;
