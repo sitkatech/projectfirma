402c4303ae06f67d9999b8ea6b9c298809b334cf
stewartgordonsitka <stewart.gordon@sitkatech.com>
2020-05-12 17:03:49 -0700

    [projectfirma/#2133] User feedback punch-list * Field definition for KMZ file

---
+++ Database/ReleaseScript/0404 - adding KMZFile field definition.sql
@@ -0,0 +1,8 @@
+INSERT [dbo].[FieldDefinition] ([FieldDefinitionID], [FieldDefinitionName], [FieldDefinitionDisplayName])
+VALUES
+(353, N'KMZFile', N'KMZ File')
+
+INSERT [dbo].[FieldDefinitionDefault] ([FieldDefinitionID], [DefaultDefinition])
+VALUES
+(353, N'KML Files may be saved using Google Earth')
+
