a05a34c2d3c25c6d0f2cc8d2b41c97842a41094c
Shannon Bulloch <shannon.bulloch@sitkatech.com>
2020-05-27 09:31:28 -0700

    [projectfirma/#2139] Pork: added SortOrder to Role table so lists of editable by or viewable roles (in grids) display the roles in the order that matches the modal editors (e.g. Unassigned before Normal User)

--- Database/LookupTables/dbo.Role.sql
+++ Database/LookupTables/dbo.Role.sql
@@ -1,10 +1,10 @@
 delete from dbo.Role
 go
 
-insert into dbo.Role(RoleID, RoleName, RoleDisplayName, RoleDescription) 
+insert into dbo.Role(RoleID, RoleName, RoleDisplayName, RoleDescription, SortOrder) 
 values 
-(1, 'Admin', 'Administrator', ''),
-(2, 'Normal', 'Normal User', 'Users with this role can propose new EIP projects, update existing EIP projects where their organization is the Lead Implementer, and view almost every page within the EIP Tracker.'),
-(7, 'Unassigned', 'Unassigned', ''),
-(8, 'SitkaAdmin', 'Sitka Administrator', ''),
-(9, 'ProjectSteward', 'Project Steward', 'Users with this role can approve Project Proposals, create new Projects, approve Project Updates, and create Funding Sources for their Organization.')
+(1, 'Admin', 'Administrator', '', 40),
+(2, 'Normal', 'Normal User', 'Users with this role can propose new EIP projects, update existing EIP projects where their organization is the Lead Implementer, and view almost every page within the EIP Tracker.', 20),
+(7, 'Unassigned', 'Unassigned', '', 10),
+(8, 'SitkaAdmin', 'Sitka Administrator', '', 50),
+(9, 'ProjectSteward', 'Project Steward', 'Users with this role can approve Project Proposals, create new Projects, approve Project Updates, and create Funding Sources for their Organization.', 30)
