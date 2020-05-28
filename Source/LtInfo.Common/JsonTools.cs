e39ddc60108576e44042a0ee59ed4381095eeea3
Shannon Bulloch <shannon.bulloch@sitkatech.com>
2020-05-28 09:06:38 -0700

    [projectfirma/#2138] getting rid of cruft

--- Source/LtInfo.Common/JsonTools.cs
+++ Source/LtInfo.Common/JsonTools.cs
@@ -38,15 +38,5 @@ namespace LtInfo.Common
         {
             return JsonConvert.SerializeObject(objectGraph, formatting);
         }
-
-        /// <summary>
-        /// see Taurus.js Taurus.Methods.decryptObfuscatedUrls- that is the inverse of this function for the javascript side.
-        /// </summary>
-        /// <param name="stringWithImgSrcUrls"></param>
-        /// <returns></returns>
-        public static string ObfuscateUrlsForMozillaPrefetching(string stringWithImgSrcUrls)
-        {
-            return stringWithImgSrcUrls != null ? stringWithImgSrcUrls.Replace("img src=\"/FileResource.mvc/GetImageFile/", "fake_E2E1F9D5-57CD-4A1F-8A1B-36B1AC4EEDB9_mozilla") : null;
-        }
     }
 }
