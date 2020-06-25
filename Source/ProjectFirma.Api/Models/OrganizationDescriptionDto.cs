1b1aead7c181c9cef85675f72473638c0333a920
Mindy Zwanziger <45750298+mindyzwan@users.noreply.github.com>
2020-06-25 13:48:01 -0700

    [delta/574] Add Organization Description specific API endpoint

---
+++ Source/ProjectFirma.Api/Models/OrganizationDescriptionDto.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ProjectFirmaModels.Models;
+
+namespace ProjectFirma.Api.Models
+{
+    public class OrganizationDescriptionDto
+    {
+        public OrganizationDescriptionDto(Organization organization)
+        {
+            OrganizationID = organization.OrganizationID;
+            Description = organization.Description;
+            FileResources = organization.OrganizationImages.Select(x => new FileResourceDto(x.FileResourceInfo))
+                .ToList();
+
+        }
+
+        public OrganizationDescriptionDto()
+        {
+        }
+
+        public int OrganizationID { get; set; }
+        public string Description { get; set; }
+        public List<FileResourceDto> FileResources { get; set; }
+
+    }
+}
