eebb293be2c5ace9a690851d2c7f1ed1baf0f002
Michael Ferrante <michael@sitkatech.com>
2020-08-06 12:20:08 -0700

    PF-2208: modify Custom Pages so they can be added to any top level menu item Adding feature attribute [AnonymousUnclassifiedFeature] to CustomPageController.ViewCustomPage() to get unit test to pass: ProjectFirma.Web.Security.FirmaFeatureTest.EachControllerActionShouldHaveOneFeature needs to be anonymous because context not derivable from url

--- Source/ProjectFirma.Web/Controllers/CustomPageController.cs
+++ Source/ProjectFirma.Web/Controllers/CustomPageController.cs
@@ -88,7 +88,7 @@ namespace ProjectFirma.Web.Controllers
             return ViewCustomPage("Configure", vanityUrl);
         }
 
-        [CustomPageViewFeature]
+        [AnonymousUnclassifiedFeature]
         public ViewResult ViewCustomPage(string route, string vanityUrl)
         {
             var customPage = MultiTenantHelpers.GetCustomPages()
