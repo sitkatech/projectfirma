ae5647597f3e472ab233419aae5017d3857a5662
Shannon Bulloch <shannon.bulloch@sitkatech.com>
2020-08-06 08:43:14 -0700

    PF-2208: combine the release scripts into one (git thought the second was a binary file, so removing it)

--- Database/ReleaseScript/0427 - Create FirmaMenuItem table and add FirmaMenuItemID to CustomPage.sql
+++ Database/ReleaseScript/0427 - Create FirmaMenuItem table and add FirmaMenuItemID to CustomPage.sql
@@ -26,3 +26,11 @@ values
 (5, 'Reports', 'Reports'),
 (6, 'Manage', 'Manage'),
 (7, 'Configure', 'Configure')
+
+alter table dbo.CustomPage add  FirmaMenuItemID int null
+alter table dbo.CustomPage add constraint FK_CustomPage_FirmaMenuItem_FirmaMenuItemID foreign key(FirmaMenuItemID) references dbo.FirmaMenuItem(FirmaMenuItemID)
+go
+
+update dbo.CustomPage set FirmaMenuItemID = 1 -- About menu
+
+alter table dbo.CustomPage alter column FirmaMenuItemID int not null
