553da25bea7ba3adac8ea32ff045aa48b22a0edb
Shannon Bulloch <shannon.bulloch@sitkatech.com>
2020-08-12 09:06:00 -0700

    PF-2208: made create/edit dialog wider so the Admin option of the viewable by section is not pushed to a second line

--- Source/ProjectFirma.Web/Views/CustomPage/CustomPageGridSpec.cs
+++ Source/ProjectFirma.Web/Views/CustomPage/CustomPageGridSpec.cs
@@ -40,7 +40,7 @@ namespace ProjectFirma.Web.Views.CustomPage
             {               
                 Add(string.Empty, x => DhtmlxGridHtmlHelpers.MakeDeleteIconAndLinkBootstrap(x.GetDeleteUrl(), true, true), 30, DhtmlxGridColumnFilterType.None);
                 Add(string.Empty, a => DhtmlxGridHtmlHelpers.MakeLtInfoEditIconAsModalDialogLinkBootstrap(new ModalDialogForm(SitkaRoute<CustomPageController>.BuildUrlFromExpression(t => t.Edit(a)),
-                        "Edit")),
+                        850, "Edit")),
                     30, DhtmlxGridColumnFilterType.None);
                 Add(string.Empty, a => DhtmlxGridHtmlHelpers.MakeModalDialogLink("<span>Edit Content</span>",
                     SitkaRoute<CustomPageController>.BuildUrlFromExpression(y => y.EditInDialog(a)),
