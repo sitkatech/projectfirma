2e97d1c4a0ca7498c748640309d75991cc84d1ea
stewartgordonsitka <stewart.gordon@sitkatech.com>
2020-08-17 12:32:04 -0700

    PF-2198 Create custom BiOp Annual Report results page * refactoring class/file location to be appropriate

---
+++ Source/ProjectFirma.Web/Models/GridColumn.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using ProjectFirmaModels.Models;
+
+namespace ProjectFirma.Web.Models
+{
+    public class GridColumn
+    {
+        public GridColumn()
+        {
+        }
+
+        public GridColumn(PersonSettingGridColumnSetting columnSetting)
+        {
+            ColumnName = columnSetting.PersonSettingGridColumn.ColumnName;
+            FilterTextArray = columnSetting.PersonSettingGridColumnSettingFilters.Select(f => f.FilterText).ToArray();
+        }
+
+        public string ColumnName { get; set; }
+        public string[] FilterTextArray { get; set; }
+        public int? Width { get; set; }
+
+
+        public bool? IsSorted { get; set; }
+        public string SortType { get; set; }
+
+    }
+}
