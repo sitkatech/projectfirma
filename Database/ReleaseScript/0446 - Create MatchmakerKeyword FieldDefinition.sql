ac3d0c71223ca38a4566578334aec418ce8e8440
StewLGSitka <stewart@sitkatech.com>
2020-09-24 17:38:40 -0700

    ProjectFirma: Cleaning up release scripts after release.

--- Database/ReleaseScript/0446 - Create MatchmakerKeyword FieldDefinition.sql
+++
@@ -1,8 +0,0 @@
-INSERT [dbo].[FieldDefinition] ([FieldDefinitionID], [FieldDefinitionName], [FieldDefinitionDisplayName])
-VALUES
-(365, N'MatchmakerKeyword', N'Matchmaker Keyword')
-
-INSERT [dbo].[FieldDefinitionDefault] ([FieldDefinitionID], [DefaultDefinition])
-VALUES
-(365, N'MatchmakerKeyword')
-
