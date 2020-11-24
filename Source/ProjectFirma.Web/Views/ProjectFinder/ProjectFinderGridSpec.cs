968dc43f6889ddd2e82420cb492dbc9404519425
tomkamin <tom.kamin@sitkatech.com>
2020-11-24 13:45:58 -0800

    PF-2277 View match score explanation increase size of popover adding field def label help icons to grid spec

--- Source/ProjectFirma.Web/Views/ProjectFinder/ProjectFinderGridSpec.cs
+++ Source/ProjectFirma.Web/Views/ProjectFinder/ProjectFinderGridSpec.cs
@@ -19,9 +19,9 @@ namespace ProjectFirma.Web.Views.ProjectFinder
             SkinRowHeight = 30;
             InitWidthsByPercentage = true;
 
-            Add(FieldDefinitionEnum.MatchScore.ToType().GetFieldDefinitionLabel(), x => x.GetMatchMakerScoreWithPopover(FieldDefinitionEnum.Project), 7, DhtmlxGridColumnFilterType.Numeric);
-            Add(FieldDefinitionEnum.Project.ToType().GetFieldDefinitionLabel(), x => x.Project.GetDisplayNameAsUrl(), 15, DhtmlxGridColumnFilterType.Html);
-            Add(FieldDefinitionEnum.ProjectStage.ToType().GetFieldDefinitionLabel(), x => x.Project.ProjectStage.ProjectStageDisplayName, 10, DhtmlxGridColumnFilterType.SelectFilterStrict);
+            Add(FieldDefinitionEnum.MatchScore.ToType().ToGridHeaderString(), x => x.GetMatchMakerScoreWithPopover(FieldDefinitionEnum.Project), 7, DhtmlxGridColumnFilterType.Numeric);
+            Add(FieldDefinitionEnum.Project.ToType().ToGridHeaderString(), x => x.Project.GetDisplayNameAsUrl(), 15, DhtmlxGridColumnFilterType.Html);
+            Add(FieldDefinitionEnum.ProjectStage.ToType().ToGridHeaderString(), x => x.Project.ProjectStage.ProjectStageDisplayName, 10, DhtmlxGridColumnFilterType.SelectFilterStrict);
             Add("Lead Implementer", x => x.Project.GetPrimaryContactOrganization().GetDisplayNameAsUrl(), 10, DhtmlxGridColumnFilterType.SelectFilterHtmlStrict);
             Add(FieldDefinitionEnum.MatchmakerKeyword.ToType().GetFieldDefinitionLabelPluralized(), x => x.ScoreInsightDictionary[MatchmakerSubScoreTypeEnum.MatchmakerKeyword].Matched.ToCheckboxImageOrEmptyForGrid(), 10, DhtmlxGridColumnFilterType.SelectFilterHtmlStrict);
             Add(FieldDefinitionEnum.AreaOfInterest.ToType().GetFieldDefinitionLabel(), x => x.ScoreInsightDictionary[MatchmakerSubScoreTypeEnum.AreaOfInterest].Matched.ToCheckboxImageOrEmptyForGrid(), 10, DhtmlxGridColumnFilterType.SelectFilterHtmlStrict);
