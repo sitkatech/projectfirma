0f64bf835d48fb2d02f77f8507e2c932e088d584
bGrass <brazen8s@gmail.com>
2021-02-16 08:21:13 -0800

    Add PSAR dashboard to Action Agenda Tracker-specific PS Info Site Explorer menu

--- Source/ProjectFirma.Web/Common/FirmaWebConfiguration.cs
+++ Source/ProjectFirma.Web/Common/FirmaWebConfiguration.cs
@@ -45,6 +45,7 @@ namespace ProjectFirma.Web.Common
         public static readonly string PsInfoDataCenterUrl = SitkaConfiguration.GetRequiredAppSetting("PsInfoDataCenterUrl");
         public static readonly string VitalSignsUrl = SitkaConfiguration.GetRequiredAppSetting("VitalSignsUrl");
         public static readonly string NEPAtlasUrl = SitkaConfiguration.GetRequiredAppSetting("NEPAtlasUrl");
+        public static readonly string PSARDashboardUrl = SitkaConfiguration.GetRequiredAppSetting("PSARDashboardUrl");
         public static readonly string PsInfoPostOrganizationUrl = SitkaConfiguration.GetRequiredAppSetting("PsInfoPostOrganizationUrl");
         public static readonly string PsInfoApiKey = SitkaConfiguration.GetRequiredAppSetting("PsInfoApiKey");
 
