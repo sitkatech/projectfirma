d6ff603cf65113115017be40b33fd7b2bc4e7c0b
bGrass <brazen8s@gmail.com>
2021-03-24 06:45:12 -0700

    DELTA-617 - resolve conflicting Organization names

---
+++ Database/Hotfixes/20210324 - DELTA-617 - resolve conflicting Organization names.sql
@@ -0,0 +1,7 @@
+
+delete from dbo.Organization
+where OrganizationID = 6509
+
+update dbo.Organization
+set OrganizationName = 'West Sound Partners for Ecosystem Recovery', OrganizationShortName = 'WSPER'
+where OrganizationID = 5798
