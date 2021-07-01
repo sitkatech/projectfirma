32cd1fc2b80e61e48379afb15a1180350fb5afb1
Brian Grass <brian.grass@sitkatech.com>
2021-07-01 16:55:21 -0700

    Update Geoserver layer name in NCRP Watersheds geospatial area type to match what's configured in Geoserver

--- Database/ReleaseScript/0484 - Copy GeospatialArea from RCD Prokect Tracker to NCRP Project Tracker.sql
+++ Database/ReleaseScript/0484 - Copy GeospatialArea from RCD Prokect Tracker to NCRP Project Tracker.sql
@@ -2,3 +2,7 @@ insert into dbo.GeospatialArea (TenantID, GeospatialAreaName, GeospatialAreaFeat
 select 4, GeospatialAreaName, GeospatialAreaFeature, 2, GeospatialAreaDescriptionContent, GeospatialAreaShortName, ExternalID from dbo.GeospatialArea where TenantID = 3
 
 update dbo.TenantAttribute set GeoServerNamespace = 'DemoProjectFirma' where TenantID = 4
+
+update dbo.GeospatialAreaType
+set GeospatialAreaLayerName = 'DemoProjectFirma:Watershed'
+where GeospatialAreaTypeID = 2
