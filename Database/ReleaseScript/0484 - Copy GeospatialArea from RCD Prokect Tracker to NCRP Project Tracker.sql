19ccb188a46e6496f6b2659604e2592ed1825d27
Shannon Bulloch <shannon.bulloch@sitkatech.com>
2021-06-30 17:10:57 -0700

    PF-2403: set GeoServerNamespace to watershed layer displays on map

--- Database/ReleaseScript/0484 - Copy GeospatialArea from RCD Prokect Tracker to NCRP Project Tracker.sql
+++ Database/ReleaseScript/0484 - Copy GeospatialArea from RCD Prokect Tracker to NCRP Project Tracker.sql
@@ -1,2 +1,4 @@
 insert into dbo.GeospatialArea (TenantID, GeospatialAreaName, GeospatialAreaFeature, GeospatialAreaTypeID, GeospatialAreaDescriptionContent, GeospatialAreaShortName, ExternalID)
 select 4, GeospatialAreaName, GeospatialAreaFeature, 2, GeospatialAreaDescriptionContent, GeospatialAreaShortName, ExternalID from dbo.GeospatialArea where TenantID = 3
+
+update dbo.TenantAttribute set GeoServerNamespace = 'DemoProjectFirma' where TenantID = 4
