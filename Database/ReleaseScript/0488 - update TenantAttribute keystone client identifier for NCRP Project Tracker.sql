4bde4a654e28ad83c94dc8262b7e89f17678dc48
Shannon Bulloch <shannon.bulloch@sitkatech.com>
2021-10-18 11:20:31 -0700

    PF-2419: updated additional TenantAttributes for NCRP Project Tracker

--- Database/ReleaseScript/0488 - update TenantAttribute keystone client identifier for NCRP Project Tracker.sql
+++ Database/ReleaseScript/0488 - update TenantAttribute keystone client identifier for NCRP Project Tracker.sql
@@ -1,4 +1,7 @@
 update dbo.TenantAttribute set KeystoneOpenIDClientIdentifier = 'NCRPProjectTracker' where TenantID = 4
+update dbo.TenantAttribute set ToolDisplayName = 'North Coast Resource Partnership Projects' where TenantID = 4
+update dbo.TenantAttribute set TenantShortDisplayName = 'Projects' where TenantID = 4
+
 update dbo.TenantAttribute set KeystoneOpenIDClientIdentifier = 'DemoProjectFirma' where TenantID = 5
 
 -- add '(Unknown or Unspecified Organization)' Organization for NCRP
