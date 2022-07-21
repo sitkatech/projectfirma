ae8cb774e1abfe3f348b7e8e15a235f0a5a78947
ShannonBullochSitka <shannon.bulloch@sitkatech.com>
2022-07-21 08:53:24 -0700

    PF-2497: NCRP's Simplified Accomplishment Dashboard includes custom funding roll-ups

---
+++ Source/ProjectFirma.Web/Views/Results/FundingSummaryViewData.cs
@@ -0,0 +1,69 @@
+﻿/*-----------------------------------------------------------------------
+<copyright file="FundingSummaryViewData.cs" company="Tahoe Regional Planning Agency and Sitka Technology Group">
+Copyright (c) Tahoe Regional Planning Agency and Sitka Technology Group. All rights reserved.
+<author>Sitka Technology Group</author>
+</copyright>
+
+<license>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License <http://www.gnu.org/licenses/> for more details.
+
+Source code is available upon request via <support@sitkatech.com>.
+</license>
+-----------------------------------------------------------------------*/
+using System.Linq;
+using ProjectFirma.Web.Common;
+using ProjectFirma.Web.Models;
+
+namespace ProjectFirma.Web.Views.Results
+{
+    public class FundingSummaryViewData : FirmaUserControlViewData
+    {
+        public decimal TotalAwarded { get; }
+        public decimal TotalMatched { get; }
+        public decimal TotalInvestment { get; }
+
+        public FundingSummaryViewData()
+        {
+            var fundingSourceCustomAttributeDictionary = HttpRequestStorage.DatabaseEntities.FundingSourceCustomAttributes
+                .GroupBy(x => x.FundingSourceID)
+                .ToDictionary(x => x.Key, y => y.ToList());
+            var fundingSourceCustomAttributeValueDictionary = HttpRequestStorage.DatabaseEntities.FundingSourceCustomAttributeValues
+                .GroupBy(x => x.FundingSourceCustomAttributeID)
+                .ToDictionary(x => x.Key, y => y.ToList());
+
+
+            // NCRP Award custom attribute
+            var fundingSourceCustomAttributeType =
+                HttpRequestStorage.DatabaseEntities.FundingSourceCustomAttributeTypes.SingleOrDefault(x =>
+                    x.FundingSourceCustomAttributeTypeName.Equals("NCRP Award"));
+
+            if (fundingSourceCustomAttributeType != null)
+            {
+                var fundingSources = HttpRequestStorage.DatabaseEntities.FundingSources.ToList();
+
+                var awardFundingSourceIDs = fundingSources.Where(x =>
+                    x.GetFundingSourceCustomAttributesValue(fundingSourceCustomAttributeType,
+                        fundingSourceCustomAttributeDictionary, fundingSourceCustomAttributeValueDictionary).Equals("Yes")).Select(x => x.FundingSourceID).ToList();
+
+                var matchedFundingSourceIDs = fundingSources.Where(x =>
+                    x.GetFundingSourceCustomAttributesValue(fundingSourceCustomAttributeType,
+                        fundingSourceCustomAttributeDictionary, fundingSourceCustomAttributeValueDictionary).Equals("No")).Select(x => x.FundingSourceID).ToList();
+
+                var projects = HttpRequestStorage.DatabaseEntities.Projects.ToList();
+
+                TotalAwarded = projects.Sum(x => x.GetSecuredFundingForFundingSources(awardFundingSourceIDs) ?? 0);
+                TotalMatched = projects.Sum(x => x.GetSecuredFundingForFundingSources(matchedFundingSourceIDs) ?? 0);
+                TotalInvestment = TotalAwarded + TotalMatched;
+            }
+
+        }
+    }
+}
