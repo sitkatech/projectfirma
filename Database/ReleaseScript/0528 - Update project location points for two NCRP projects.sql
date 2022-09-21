0bd8629789d00a4b68fc55424d03d19de1a972ca
ShannonBullochSitka <shannon.bulloch@sitkatech.com>
2022-09-21 15:43:23 -0700

    PF-2516: update location of 2 projects

---
+++ Database/ReleaseScript/0528 - Update project location points for two NCRP projects.sql
@@ -0,0 +1,2 @@
+update dbo.Project set ProjectLocationPoint = geometry::Point(-123.89692, 41.77911, 4326), ProjectLocationSimpleTypeID = 2 where TenantID = 4 and ProjectID = 14346
+update dbo.Project set ProjectLocationPoint = geometry::Point(-123.14047, 41.14252, 4326), ProjectLocationSimpleTypeID = 2 where TenantID = 4 and ProjectID = 16627
