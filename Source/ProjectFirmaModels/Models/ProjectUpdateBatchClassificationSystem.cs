cc0541f1a6fd635bd7c61e52325633f5accd659c
ShannonBullochSitka <shannon.bulloch@sitkatech.com>
2023-01-23 19:57:46 -0800

    Bug fix: Added ProjectUpdateBatchClassificationSystem.cs to implement IAuditableEntity so project update batches (and project that have update batches) can be deleted without error.

---
+++ Source/ProjectFirmaModels/Models/ProjectUpdateBatchClassificationSystem.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectFirmaModels.Models
+{
+    public partial class ProjectUpdateBatchClassificationSystem : IAuditableEntity
+    {
+        public string GetAuditDescriptionString()
+        {
+            return $"ProjectUpdateBatchClassificationSystemID: {ProjectUpdateBatchClassificationSystemID}, ProjectUpdateBatch: {ProjectUpdateBatchID}, ClassificationSystem: {ClassificationSystemID}";
+        }
+    }
+}
