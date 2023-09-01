caa41d66b6dd64b0dba94c7f62b801a0cfb165f3
tomkamin <tom.kamin@sitkatech.com>
2023-09-01 16:09:37 -0700

    adding FKs to get unit test to pass making sure tables have dual FKs with Tenant table

--- Database/Tables/dbo.GeospatialAreaType.sql
+++ Database/Tables/dbo.GeospatialAreaType.sql
@@ -40,6 +40,11 @@ GO
 ALTER TABLE [dbo].[GeospatialAreaType]  WITH CHECK ADD FOREIGN KEY([MapLegendImageFileResourceInfoID])
 REFERENCES [dbo].[FileResourceInfo] ([FileResourceInfoID])
 GO
+ALTER TABLE [dbo].[GeospatialAreaType]  WITH CHECK ADD  CONSTRAINT [FK_GeospatialAreaType_FileResourceInfo_MapLegendImageFileResourceInfoID_TenantID] FOREIGN KEY([MapLegendImageFileResourceInfoID], [TenantID])
+REFERENCES [dbo].[FileResourceInfo] ([FileResourceInfoID], [TenantID])
+GO
+ALTER TABLE [dbo].[GeospatialAreaType] CHECK CONSTRAINT [FK_GeospatialAreaType_FileResourceInfo_MapLegendImageFileResourceInfoID_TenantID]
+GO
 ALTER TABLE [dbo].[GeospatialAreaType]  WITH CHECK ADD  CONSTRAINT [FK_GeospatialAreaType_Tenant_TenantID] FOREIGN KEY([TenantID])
 REFERENCES [dbo].[Tenant] ([TenantID])
 GO
