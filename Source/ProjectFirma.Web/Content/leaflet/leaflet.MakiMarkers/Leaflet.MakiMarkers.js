3d662548ccb7e3f7417500b19fe452725f1f8737
tomkamin <tom.kamin@sitkatech.com>
2023-12-13 15:51:58 -0800

    DELTA-1107 Fix NEP Atlas Map Marker Icons updating MakiMarker library to use latest mapbox version, because the previous one was deprecated. added MapBoxApiKey so we can use the latest version of mapbox(v4)

--- Source/ProjectFirma.Web/Content/leaflet/leaflet.MakiMarkers/Leaflet.MakiMarkers.js
+++ Source/ProjectFirma.Web/Content/leaflet/leaflet.MakiMarkers/Leaflet.MakiMarkers.js
@@ -3,9 +3,11 @@
  *
  * References:
  *   Maki Icons: https://www.mapbox.com/maki/
- *   MapBox Marker API: https://www.mapbox.com/developers/api/static/#markers
+ *   Mapbox Marker API: https://www.mapbox.com/api-documentation/#retrieve-a-standalone-marker
+ *   Possible icon names: https://raw.githubusercontent.com/mapbox/maki/master/layouts/all.json
  *
  * Usage:
+ *   L.MakiMarkers.accessToken = "<YOUR_ACCESS_TOKEN>";
  *   var icon = L.MakiMarkers.icon({icon: "rocket", color: "#b0b", size: "m"});
  *
  * License:
@@ -15,27 +17,11 @@
 (function () {
   "use strict";
   L.MakiMarkers = {
-    // Available Maki Icons
-    icons: ["airfield","airport","alcohol-shop","america-football","art-gallery","bakery","bank","bar",
-      "baseball","basketball","beer","bicycle","building","bus","cafe","camera","campsite","car",
-      "cemetery","chemist","cinema","circle-stroked","circle","city","clothing-store","college",
-      "commercial","cricket","cross","dam","danger","disability","dog-park","embassy",
-      "emergency-telephone","entrance","farm","fast-food","ferry","fire-station","fuel","garden",
-      "golf","grocery","hairdresser","harbor","heart","heliport","hospital","industrial",
-      "land-use","laundry","library","lighthouse","lodging","logging","london-underground",
-      "marker-stroked","marker","minefield","mobilephone","monument","museum","music","oil-well",
-      "park2","park","parking-garage","parking","pharmacy","pitch","place-of-worship",
-      "playground","police","polling-place","post","prison","rail-above","rail-light",
-      "rail-metro","rail-underground","rail","religious-christian","religious-jewish",
-      "religious-muslim","restaurant","roadblock","rocket","school","scooter","shop","skiing",
-      "slaughterhouse","soccer","square-stroked","square","star-stroked","star","suitcase",
-      "swimming","telephone","tennis","theatre","toilets","town-hall","town","triangle-stroked",
-      "triangle","village","warehouse","waste-basket","water","wetland","zoo"
-    ],
+      accessToken: null,
     defaultColor: "#0a0",
     defaultIcon: "circle-stroked",
     defaultSize: "m",
-    apiUrl: "https://api.tiles.mapbox.com/v3/marker/",
+    apiUrl: "https://api.mapbox.com/v4/marker/",
     smallOptions: {
       iconSize: [20, 50],
       popupAnchor: [0,-20]
@@ -52,7 +38,10 @@
 
   L.MakiMarkers.Icon = L.Icon.extend({
     options: {
-      //Maki icon: any from https://www.mapbox.com/maki/ (ref: L.MakiMarkers.icons)
+      //Mapbox API access token, see https://www.mapbox.com/api-documentation/?language=CLI#access-tokens
+      //Instead of setting with each icon, you can set globally as L.MakiMarkers.accessToken
+      accessToken: null,
+      //Maki icon: any valid name, see https://raw.githubusercontent.com/mapbox/maki/master/layouts/all.json
       icon: L.MakiMarkers.defaultIcon,
       //Marker color: short or long form hex color code
       color: L.MakiMarkers.defaultColor,
@@ -66,7 +55,14 @@
 
     initialize: function(options) {
       var pin;
+      var tokenQuery;
 
+      var accessToken = options.accessToken || L.MakiMarkers.accessToken;
+      if (!accessToken) {
+        throw new Error("Access to the Mapbox API requires a valid access token.");
+      }
+
+      tokenQuery = "?access_token=" + accessToken;
       options = L.setOptions(this, options);
 
       switch (options.size) {
@@ -82,7 +78,6 @@
           break;
       }
 
-
       pin = "pin-" + options.size;
 
       if (options.icon !== null) {
@@ -97,8 +92,8 @@
         pin += "+" + options.color;
       }
 
-      options.iconUrl = "" + L.MakiMarkers.apiUrl + pin +  ".png";
-      options.iconRetinaUrl = L.MakiMarkers.apiUrl + pin + "@2x.png";
+      options.iconUrl = "" + L.MakiMarkers.apiUrl + pin +  ".png" + tokenQuery;
+      options.iconRetinaUrl = L.MakiMarkers.apiUrl + pin + "@2x.png" + tokenQuery;
     }
   });
 
