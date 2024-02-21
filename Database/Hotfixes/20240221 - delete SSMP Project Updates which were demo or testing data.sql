cede3f2eba58b144f7a93a98695e44f9142fe781
Shannon Bulloch <sbulloch@esassoc.com>
2024-02-21 15:47:03 -0800

    hotfix to delete SSMP project updates (they were all demo/testing data and should not be kept after go live)

---
+++ Database/Hotfixes/20240221 - delete SSMP Project Updates which were demo or testing data.sql
@@ -0,0 +1,21 @@
+
+delete from dbo.ProjectUpdate where TenantID = 13
+delete from dbo.ProjectOrganizationUpdate where TenantID = 13
+delete from dbo.ProjectUpdateHistory where TenantID = 13
+delete from dbo.ProjectImageUpdate where TenantID = 13
+delete from dbo.ProjectUpdateBatchClassificationSystem where TenantID = 13
+delete from dbo.ProjectClassificationUpdate where TenantID = 13
+delete from dbo.ProjectCustomAttributeUpdateValue where TenantID = 13
+delete from dbo.ProjectCustomAttributeUpdate where TenantID = 13
+delete from dbo.ProjectClassificationUpdate where TenantID = 13
+
+delete from dbo.ProjectGeospatialAreaUpdate where TenantID = 13
+delete from dbo.ProjectLocationUpdate where TenantID = 13
+delete from dbo.PerformanceMeasureExpectedSubcategoryOptionUpdate where TenantID = 13
+delete from dbo.PerformanceMeasureExpectedUpdate where TenantID = 13
+delete from dbo.PerformanceMeasureActualSubcategoryOptionUpdate where TenantID = 13
+delete from dbo.PerformanceMeasureActualUpdate where TenantID = 13
+delete from dbo.ProjectExemptReportingYearUpdate where TenantID = 13
+delete from dbo.ProjectAttachmentUpdate where TenantID = 13
+delete from dbo.ProjectContactUpdate where TenantId = 13
+delete from dbo.ProjectUpdateBatch where TenantID = 13
