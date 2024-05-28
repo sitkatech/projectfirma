0e72dfcf62a9143e12deb68609e8fe5aa6fcfa0c
Rex Ounekeo <rounekeo@esassoc.com>
2024-05-28 13:06:00 -0700

    PF-2687 Adjust the styling of the TCS Project Tracker Geospatial Areas - Updated GeospatialArea.LayerColor for National Forest areas and "Not in a WUI Zone"

---
+++ Database/ReleaseScript/0571 - Updating National Forest GeospatialArea-LayerColor for TCS.sql
@@ -0,0 +1,29 @@
+update ProjectFirma.dbo.GeospatialArea 
+set LayerColor = '#5C8944A0'
+where TenantID = 14
+	and GeospatialAreaName = 'Eldorado National Forest'
+	
+update ProjectFirma.dbo.GeospatialArea 
+set LayerColor = '#446589A0'
+where TenantID = 14
+	and GeospatialAreaName = 'Lake Tahoe Basin Management Unit'
+	
+update ProjectFirma.dbo.GeospatialArea 
+set LayerColor = '#59acffA0'
+where TenantID = 14
+	and GeospatialAreaName = 'Not in a National Forest'
+	
+update ProjectFirma.dbo.GeospatialArea 
+set LayerColor = '#A46547A0'
+where TenantID = 14
+	and GeospatialAreaName = 'Plumas National Forest'
+	
+update ProjectFirma.dbo.GeospatialArea 
+set LayerColor = '#A8A800A0'
+where TenantID = 14
+	and GeospatialAreaName = 'Tahoe National Forest'
+	
+update ProjectFirma.dbo.GeospatialArea 
+set LayerColor = '#59acffA0'
+where TenantID = 14
+	and GeospatialAreaName = 'Not in a WUI Zone'
