f774fd4366bd1e28fd899de19be8f82027eccc86
tomkamin <tom.kamin@sitkatech.com>
2024-11-13 18:57:41 -0800

    clearing out release scripts that have made it to all prod environments

--- Database/ReleaseScript/0538 - add ReportFinancialsAtProjectLevel to TenantAttribute table.sql
+++
@@ -1,6 +0,0 @@
-alter table dbo.TenantAttribute add ReportFinancialsAtProjectLevel bit null
-go
-update dbo.TenantAttribute set ReportFinancialsAtProjectLevel = 1
-
-alter table dbo.TenantAttribute alter column ReportFinancialsAtProjectLevel bit not null
-
--- Database/ReleaseScript/0544 - add more comments columns to ProjectUpdateBatch.sql
+++
@@ -1,5 +0,0 @@
-alter table dbo.ProjectUpdateBatch add PhotosComment varchar(1000)
-alter table dbo.ProjectUpdateBatch add AttachmentsAndNotesComment varchar(1000)
-alter table dbo.ProjectUpdateBatch add ExternalLinksComment varchar(1000)
-
-alter table dbo.Project add ExternalLinksComment varchar(1000)
